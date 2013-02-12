95f27e35861ecf8b7432853a59e8e17ad1a29fe8
Dave Landry <dave@growth-ventures.com>
2013-02-12 17:07:31 -0500

    uploaded rudimentary network app and modernizr touch events

--- src/general.js
+++ src/general.js
@@ -8,3 +8,21 @@ window.vizwhiz = vizwhiz;
 vizwhiz.tooltip = {}; // For the tooltip system
 vizwhiz.utils = {}; // Utility subsystem
 vizwhiz.viz = {}; //stores all the possible visualizations
+vizwhiz.evt = {}; // stores all mouse events that could occur
+
+// Modernizr touch events
+if (Modernizr.touch) {
+  vizwhiz.evt.click = 'touchend'
+  vizwhiz.evt.down = 'touchstart'
+  vizwhiz.evt.up = 'touchend'
+  vizwhiz.evt.over = 'touchstart'
+  vizwhiz.evt.out = 'touchend'
+  vizwhiz.evt.move = 'touchmove'
+} else {
+  vizwhiz.evt.click = 'click'
+  vizwhiz.evt.down = 'mousedown'
+  vizwhiz.evt.up = 'mouseup'
+  vizwhiz.evt.over = 'mouseover'
+  vizwhiz.evt.out = 'mouseout'
+  vizwhiz.evt.move = 'mousemove'
+}
