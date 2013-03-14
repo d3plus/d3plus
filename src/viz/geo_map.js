aba67fb3e699e6c4a0adb060ed1cae245a9a04a9
Dave Landry <dave@growth-ventures.com>
2013-03-14 08:12:28 -0400

    changed "tooltip" to "tooltip_info" to match other apps

--- src/viz/geo_map.js
+++ src/viz/geo_map.js
@@ -627,7 +627,7 @@ vizwhiz.viz.geo_map = function() {
     return chart;
   };
   
-  chart.tooltip = function(x) {
+  chart.tooltip_info = function(x) {
     if (!arguments.length) return tooltip_info;
     tooltip_info = x;
     return chart;
