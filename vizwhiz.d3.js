4a1f1604b760424cafb7d0ea4f41d47ea55402ef
Dave Landry <dave@growth-ventures.com>
2013-03-20 13:32:08 -0400

    includes pie scatter and bubbles arctween fix

--- vizwhiz.d3.js
+++ vizwhiz.d3.js
@@ -3271,7 +3271,9 @@ vizwhiz.viz.pie_scatter = function() {
     nesting = [],
     filter = [],
     solo = [],
-    tooltip_info = [];
+    tooltip_info = [],
+    arc_angles = {},
+    arc_sizes = {};
   
   //===================================================================
   
@@ -3296,7 +3298,7 @@ vizwhiz.viz.pie_scatter = function() {
       // first clone input so we know we are working with fresh data
       // var cloned_data = JSON.parse(JSON.stringify(data));
       var nested_data = data;
-
+      
       // update size
       size.width = width-margin.left-margin.right;
       size.height = height-margin.top-margin.bottom;
@@ -3345,7 +3347,7 @@ vizwhiz.viz.pie_scatter = function() {
         .domain(d3.extent(nested_data, function(d){ return d[xaxis_var]; }))
         .range([0, size.width])
         .nice()
-      
+        
       // get buffer room (take into account largest size var)
       var inverse_x_scale = d3.scale.linear().domain(x_scale.range()).range(x_scale.domain())
       var largest_size = size_scale.range()[1]
@@ -3355,7 +3357,6 @@ vizwhiz.viz.pie_scatter = function() {
       var x_buffer = largest_size - x_scale.domain()[0];
       // update x scale with new buffer offsets
       x_scale.domain([x_scale.domain()[0]-x_buffer, x_scale.domain()[1]+x_buffer])
-      console.log(largest_size)
       // enter
       var xaxis_enter = viz_enter.append("g")
         .attr("transform", "translate(0," + size.height + ")")
@@ -3450,6 +3451,8 @@ vizwhiz.viz.pie_scatter = function() {
       var arc = d3.svg.arc()
         .innerRadius(0)
         .startAngle(0)
+        .outerRadius(function(d) { return d.arc_radius })
+        .endAngle(function(d) { return d.arc_angle })
       
       var has_children = nested_data[0].num_children ? true : false;
       
@@ -3495,7 +3498,7 @@ vizwhiz.viz.pie_scatter = function() {
         .selectAll("g.circle")
         .data(nested_data, function(d){ return d.name; })
       
-      var nodes_enter = nodes.enter().append("g")
+      nodes.enter().append("g")
         .attr("opacity", 0)
         .attr("class", "circle")
         .attr("transform", function(d) { return "translate("+x_scale(d[xaxis_var])+","+y_scale(d[yaxis_var])+")" } )
@@ -3504,41 +3507,49 @@ vizwhiz.viz.pie_scatter = function() {
           vizwhiz.tooltip.remove();
           d3.selectAll(".axis_hover").remove();
         })
-      
-      nodes_enter
-        .append("circle")
-        .style('stroke', function(d){ return d.color })
-        .style('stroke-width', 3)
-        .style('fill', function(d){ return d.color })
-        .style("fill-opacity", function(d) {
-          return d.active ? 0.75 : 0.25;
+        .each(function(d){
+          
+          d3.select(this)
+            .append("circle")
+            .style('stroke', d.color )
+            .style('stroke-width', 3)
+            .style('fill', d.color )
+            .style("fill-opacity", function(dd) { return d.active ? 0.75 : 0.25; })
+            .attr("r", 0 )
+            
+          arc_angles[d.id] = 0
+          arc_sizes[d.id] = 0
+            
+          d3.select(this)
+            .append("path")
+            .style('fill', d.color )
+            .style("fill-opacity", 1)
+            
+          d3.select(this).select("path").transition().duration(vizwhiz.timing)
+            .attrTween("d",arcTween)
+            
         })
-        .attr("r", function(d){ 
-          return size_scale(d[value_var]);
-        })
-
-      nodes_enter
-        .append("path")
-        .style('fill', function(d){ return d.color })
-        .style("fill-opacity", 1)
       
       // update
       nodes.transition().duration(vizwhiz.timing)
         .attr("transform", function(d) { return "translate("+x_scale(d[xaxis_var])+","+y_scale(d[yaxis_var])+")" } )
         .attr("opacity", 1)
-      
-      nodes.selectAll("circle").transition().duration(vizwhiz.timing)
-        .style('fill', function(d){ return d.color })
-        .attr("r", function(d){ return size_scale(d[value_var]); })
-      
-      nodes.selectAll("path").transition().duration(vizwhiz.timing)
-        .attr("d", function(d){
-          var angle = 0, radius = 0;
-          if(d.num_children){
-            angle = (((d.num_children_active / d.num_children)*360) * (Math.PI/180));
-            radius = size_scale(d[value_var]);
-          }
-          return arc.endAngle(angle).outerRadius(radius)(d);
+        .each(function(d){
+
+          d.arc_radius = size_scale(d[value_var]);
+          if (d.num_children) d.arc_angle = (((d.num_children_active / d.num_children)*360) * (Math.PI/180));
+          
+          d3.select(this).select("circle").transition().duration(vizwhiz.timing)
+            .style('fill', d.color )
+            .attr("r", d.arc_radius )
+          
+          d3.select(this).select("path").transition().duration(vizwhiz.timing)
+            .attrTween("d",arcTween)
+            .each("end", function(dd) {
+              arc_angles[d.id] = d.arc_angle
+              arc_sizes[d.id] = d.arc_radius
+            })
+          
         })
       
       // exit
@@ -3620,6 +3631,13 @@ vizwhiz.viz.pie_scatter = function() {
       
       // Always bring to front
       d3.select("rect.border").node().parentNode.appendChild(d3.select("rect.border").node())
+      
+      function arcTween(b) {
+        var i = d3.interpolate({arc_angle: arc_angles[b.id], arc_radius: arc_sizes[b.id]}, b);
+        return function(t) {
+          return arc(i(t));
+        };
+      }
       
     });
 
@@ -3769,7 +3787,7 @@ vizwhiz.viz.pie_scatter = function() {
         .attr("y2", 20)
         .attr("stroke", "#000")
         .attr("stroke-width", stroke)
-      return d
+      return vizwhiz.utils.format_num(d, false, 3, true);
     });
   
   var y_axis = d3.svg.axis()
@@ -3810,7 +3828,7 @@ vizwhiz.viz.pie_scatter = function() {
         .attr("stroke", "#4c4c4c")
         .attr("stroke-width",stroke)
       // return parseFloat(d.toFixed(3))
-      return vizwhiz.utils.format_num(d, false, 3, true)
+      return vizwhiz.utils.format_num(d, false, 3, true);
     });
 
   //===================================================================
@@ -3952,7 +3970,9 @@ vizwhiz.viz.bubbles = function() {
       id_var = "id",
       text_var = "name",
       grouping = "name",
-      tooltip_info = [];
+      tooltip_info = []
+      arc_angles = {},
+      arc_sizes = {};
 
   //===================================================================
 
@@ -3969,7 +3989,7 @@ vizwhiz.viz.bubbles = function() {
           stroke_width = 1,
           groups = {},
           value_extent = d3.extent(d3.values(data),function(d){ return d[value_var]; }),
-            value_map = d3.scale.linear().domain(value_extent).range([1,4]);
+          value_map = d3.scale.linear().domain(value_extent).range([1,4]);
 
       //===================================================================
       
@@ -4120,13 +4140,34 @@ vizwhiz.viz.bubbles = function() {
       var arc = d3.svg.arc()
         .innerRadius(0)
         .startAngle(0)
+        .outerRadius(function(d) { return d.arc_radius })
+        .endAngle(function(d) { return d.arc_angle })
 
       var bubble = d3.select("g.bubbles").selectAll("g.bubble")
         .data(data, function(d) { return d[id_var] })
         
-      var bubble_enter = bubble.enter().append("g")
+      bubble.enter().append("g")
         .attr("class", "bubble")
         .attr("transform", function(d){ return "translate("+d.cx+","+d.cy+")"; })
+        .each(function(d){
+          
+          d3.select(this).append("circle")
+            .attr("fill", d.color )
+            .style('fill-opacity', 0.25 )
+            .attr("r",0)
+            .attr("stroke-width",2)
+            .attr("stroke", d.color );
+            
+          arc_angles[d.id] = 0
+          arc_sizes[d.id] = 0
+            
+          d3.select(this).append("path")
+            .style('fill', d.color )
+            
+          d3.select(this).select("path").transition().duration(vizwhiz.timing)
+            .attrTween("d",arcTween)
+            
+        })
         .on(vizwhiz.evt.over, function(d){
           
           var tooltip_data = {}
@@ -4148,23 +4189,6 @@ vizwhiz.viz.bubbles = function() {
         })
         .on(vizwhiz.evt.out, function(d){
           vizwhiz.tooltip.remove(d[id_var])
-        });
-        
-      bubble_enter.append("circle")
-        .attr("fill", function(d){ return d.color; })
-        .attr("r",0)
-        .attr("stroke-width",2)
-        .attr("stroke", function(d){ return d.color; });
-        
-      bubble_enter.append("path")
-        .attr("fill", function(d){ return d.color; })
-        .attr("d", function(d){
-          if(d.total){
-            var angle = (((d.available / d.total)*360) * (Math.PI/180));
-          } else if (d.active) {
-            var angle = 360; 
-          } else var angle = 0;
-          return arc.endAngle(angle).outerRadius(0)(d);
         });
       
       var label = d3.select("g.labels").selectAll("text")
@@ -4214,27 +4238,23 @@ vizwhiz.viz.bubbles = function() {
       // Update, for things that are already in existance
       //-------------------------------------------------------------------
         
-      bubble.selectAll("circle").transition().duration(timing)
-        .attr("r", function(d){ return d.radius; })
-        .style('fill-opacity', 0.25 );
-        
-      bubble.selectAll("path").transition().duration(timing)
-        .attr("d", function(d){
-          if(d.total){
-            var angle = (((d.available / d.total)*360) * (Math.PI/180));
-          } else if (d.active) {
-            var angle = 360; 
-          } else var angle = 0;
-          return arc.endAngle(angle).outerRadius(d.radius)(d);
-        });
-        
-      label.transition().duration(timing/2)
-        .attr("opacity",1)
-        .attr('x',function(d) { return d.x; })
-        .attr('y',function(d) {
-          if (Object.keys(groups).length == 2) var y_offset = height
-          else var y_offset = d3.min([d.width,d.height]);
-          return d.y+(y_offset/2)-30;
+      bubble.transition().duration(vizwhiz.timing)
+        .each(function(d){
+          
+          d.arc_radius = d.radius;
+          if (d.total) d.arc_angle = (((d.available / d.total)*360) * (Math.PI/180));
+          else if (d.active) d.arc_angle = 360; 
+          
+          d3.select(this).select("circle").transition().duration(vizwhiz.timing)
+            .attr("r", d.arc_radius )
+          
+          d3.select(this).select("path").transition().duration(vizwhiz.timing)
+            .attrTween("d",arcTween)
+            .each("end", function(dd) {
+              arc_angles[d.id] = d.arc_angle
+              arc_sizes[d.id] = d.arc_radius
+            })
+          
         })
         
       svg.transition().duration(timing)
@@ -4319,6 +4339,13 @@ vizwhiz.viz.bubbles = function() {
               || x2 < nx1
               || y1 > ny2
               || y2 < ny1;
+        };
+      }
+      
+      function arcTween(b) {
+        var i = d3.interpolate({arc_angle: arc_angles[b.id], arc_radius: arc_sizes[b.id]}, b);
+        return function(t) {
+          return arc(i(t));
         };
       }
 
