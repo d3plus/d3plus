265a9351782df90ebfd570b6c293f9980bef9311
Dave Landry <landry.dave@gmail.com>
2014-02-07 14:12:25 -0500

    re-compiled v1.1.3

--- d3plus.js
+++ d3plus.js
@@ -5674,7 +5674,75 @@ d3plus.forms.drop = function(vars,styles,timing) {
     .tween("scroll",scrollTopTween(scroll))
   
 }
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Creates a set of Radio Buttons
+//------------------------------------------------------------------------------
+d3plus.forms.radio = function(vars,styles,timing) {
+  
+  vars.container.transition().duration(timing)
+    .style("border-style","solid")
+    .style("border-width",styles.stroke+"px")
+    .style("border-color",styles.color)
+    // .style("background-color",styles.color)
+    // .style("padding",styles.padding+"px")
+    
+  var button_style = d3plus.utils.copy(styles)
+  button_style.icon = false
+  button_style.display = "inline-block"
+  button_style.border = "none"
+  button_style.width = false
+  button_style.margin = 0
+  
+  var text = d3plus.forms.value(vars.text,["button"])
+  if (!text) {
+   text = "text"
+  }
+  
+  var buttons = []
+  vars.data.forEach(function(d,i){
+
+    var button = d3plus.ui(button_style)
+      .type("button")
+      .text(text)
+      .data([d])
+      .parent(vars.container)
+      .id(vars.id+"_option"+i)
+      .callback(vars.ui.value)
+      
+    if (vars.focus == d.value) {
+      button.enable()
+    }
+      
+    button
+      .draw()
+      
+    buttons.push(button)
+      
+  })
+  
+}
+d3plus.forms.value = function(obj,arr) {
+  
+  if (typeof obj == "object" && arr) {
+    var ret = false
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] in obj) {
+        ret = obj[arr[i]]
+        break;
+      }
+    }
+    if (ret) {
+      return ret
+    }
+  }
+  else if (typeof obj != "object") {
+    return obj
+  }
+  else {
+    return false
+  }
+  
+}//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Creates Error Message
 //-------------------------------------------------------------------
 
