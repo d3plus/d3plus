25247f07a0941f34cb4ae230121a23dd2420a116
Dave Landry <landry.dave@gmail.com>
2014-02-10 18:03:57 -0500

    support new bounds fitting

--- src/zoom/bounds.js
+++ src/zoom/bounds.js
@@ -2,14 +2,24 @@ d3plus.zoom.bounds = function(vars,b) {
   
   vars.viewport = b
   
-  var aspect = Math.max((b[1][0]-b[0][0])/vars.app_width, (b[1][1]-b[0][1])/vars.app_height)
+  if (vars.coords.fit.value == "auto") {
+    var aspect = Math.max((b[1][0]-b[0][0])/vars.app_width, (b[1][1]-b[0][1])/vars.app_height)
+  }
+  else {
+    var w = vars.coords.fit.value == "width" ? b[1][0]-b[0][0] : b[1][1]-b[0][1],
+        aspect = w/vars["app_"+vars.coords.fit.value]
+  }
   
-  vars.zoom.scale = .95 / aspect
+  var min = d3.min([vars.app_width,vars.app_height])
   
-  vars.zoom.translate = [
+  vars.zoom.scale = ((min-(vars.coords.padding*2)) / min) / aspect
+  
+  var translate = [
     -(b[1][0] + b[0][0]) / 2,
     -(b[1][1] + b[0][1]) / 2
   ]
+  
+  vars.zoom.translate = translate
       
   vars.g.zoom
     .attr("transform","translate(" + [vars.app_width/2,vars.app_height/2] + ")"
