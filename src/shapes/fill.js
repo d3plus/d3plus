9117d1728f46d7607701ca058ce1bf8a9cdb2838
Dave Landry <landry.dave@gmail.com>
2014-02-20 23:09:29 -0500

    re-wrote fill code to fix some edge case bugs

--- src/shapes/fill.js
+++ src/shapes/fill.js
@@ -115,6 +115,7 @@ d3plus.shape.fill = function(vars,selection,enter,exit) {
       }
       var radius = d3.interpolate(vars.arcs[d.d3plus.shapeType][d.d3plus.id].r,r+mod),
           angle = d3.interpolate(vars.arcs[d.d3plus.shapeType][d.d3plus.id].a,a)
+          
       return function(t) {
         vars.arcs[d.d3plus.shapeType][d.d3plus.id].r = radius(t)
         vars.arcs[d.d3plus.shapeType][d.d3plus.id].a = angle(t)
@@ -131,161 +132,121 @@ d3plus.shape.fill = function(vars,selection,enter,exit) {
     var active = vars.active.key ? d.d3plus[vars.active.key] : d.d3plus.active,
         temp = vars.temp.key ? d.d3plus[vars.temp.key] : d.d3plus.temp,
         total = vars.total.key ? d.d3plus[vars.total.key] : d.d3plus.total,
-        group = d3.select(this)
-        
-    function destroy(type) {
-      
-      var r = d.d3plus.r ? d.d3plus.r : d3.max([d.d3plus.width,d.d3plus.height])
+        group = d3.select(this),
+        color = d3plus.variable.color(vars,d)
     
-      group.selectAll("path.d3plus_"+type)
-        .transition().duration(vars.style.timing.transitions)
-        .call(size,0,r,0)
-        .remove()
-    
-    }
-  
-    function create(type) {
-
-      var new_data = d3plus.utils.copy(d)
-      new_data.d3plus.shapeType = type
-    
-      var delay = 0
-
-      var color = d3plus.variable.color(vars,d)
-    
-      if (group.selectAll("path.d3plus_"+type).empty()) {
-          
-        delay = vars.style.timing.transitions
-      
-        if (type == "temp") {
-        
-          var pattern = vars.defs.append("pattern")
-            .attr("id","hatch"+d.d3plus.id)
-            .attr("patternUnits","userSpaceOnUse")
-            .attr("x","0")
-            .attr("y","0")
-            .attr("width","10")
-            .attr("height","10")
-            .append("g")
-          
-          pattern.append("rect")
-            .attr("x","0")
-            .attr("y","0")
-            .attr("width","10")
-            .attr("height","10")
-            .attr("fill",color)
-            .attr("fill-opacity",0.25)
-          
-          pattern.append("line")
-            .attr("x1","0")
-            .attr("x2","10")
-            .attr("y1","0")
-            .attr("y2","10")
-            .attr("stroke",color)
-            .attr("stroke-width",1)
-            .attr("shape-rendering",vars.style.rendering)
-          
-          pattern.append("line")
-            .attr("x1","-1")
-            .attr("x2","1")
-            .attr("y1","9")
-            .attr("y2","11")
-            .attr("stroke",color)
-            .attr("stroke-width",1)
-            .attr("shape-rendering",vars.style.rendering)
-          
-          pattern.append("line")
-            .attr("x1","9")
-            .attr("x2","11")
-            .attr("y1","-1")
-            .attr("y2","1")
-            .attr("stroke",color)
-            .attr("stroke-width",1)
-            .attr("shape-rendering",vars.style.rendering)
-        }
-      
-        if (group.selectAll("#clip_"+d.d3plus.id).empty()) {
-          group.insert("clipPath",".d3plus_mouse")
-            .attr("id","clip_"+d.d3plus.id)
-            .append("rect")
-              .attr("class","d3plus_clipping")
-              .call(init)
-              .transition().duration(vars.style.timing.transitions)
-                .call(update)
-        }
-      
-        var r = d.d3plus.r ? d.d3plus.r : d3.max([d.d3plus.width,d.d3plus.height])
-      
-        group.insert("path","rect.d3plus_mouse")
-          .attr("class","d3plus_"+type)
-          .attr("clip-path","url(#clip_"+d.d3plus.id+")")
-          .data([new_data])
-          .transition().duration(0)
-            .call(size,0,r,0)
-            .call(d3plus.shape.style,vars)
-            .transition().duration(delay)
-              .call(size)
-            
-      }
-
-      group.selectAll("path.d3plus_"+type)
-        .data([new_data])
-        .transition().duration(vars.style.timing.transitions)
-        .delay(delay)
-        .call(size)
-        .call(d3plus.shape.style,vars)
-      
-      group.selectAll("#clip_"+d.d3plus.id)
-        .select("rect")
-        .data([new_data])
-        .transition().duration(vars.style.timing.transitions)
-        .delay(delay)
-          .call(update)
-        
-      vars.defs.selectAll("pattern#hatch"+d.d3plus.id).selectAll("rect")
-        .transition().duration(vars.style.timing.transitions)
-        .delay(delay)
-        .style("fill",color)
-    
-      vars.defs.selectAll("pattern#hatch"+d.d3plus.id).selectAll("line")
-        .transition().duration(vars.style.timing.transitions)
-        .delay(delay)
-        .style("stroke",color)
-        
-    }
+    var fill_data = [], hatch_data = []
       
     if (total && d3plus.apps[vars.type.value].fill) {
       
-      if (active && active < total) {
-        create("active")
-      }
-      else {
-        destroy("active")
+      if (temp) {
+        var copy = d3plus.utils.copy(d)
+        copy.d3plus.shapeType = "temp"
+        fill_data.push(copy)
+        hatch_data = ["temp"]
       }
       
-      if (temp) {
-        create("temp")
-      }
-      else {
-        destroy("temp")
-      }
-    
-      if (!temp && !active) {
-        group.selectAll("#clip_"+d.d3plus.id)
-          .transition().delay(vars.style.timing.transitions)
-          .remove()
+      if (active && (active < total || temp)) {
+        var copy = d3plus.utils.copy(d)
+        copy.d3plus.shapeType = "active"
+        fill_data.push(copy)
       }
         
     }
-    else {
+    
+    var pattern = vars.defs.selectAll("pattern#d3plus_hatch_"+d.d3plus.id)
+      .data(hatch_data)
+  
+    pattern.selectAll("rect")
+      .transition().duration(vars.style.timing.transitions)
+      .style("fill",color)
+
+    pattern.selectAll("line")
+      .transition().duration(vars.style.timing.transitions)
+      .style("stroke",color)
+  
+    var pattern_enter = pattern.enter().append("pattern")
+      .attr("id","d3plus_hatch_"+d.d3plus.id)
+      .attr("patternUnits","userSpaceOnUse")
+      .attr("x","0")
+      .attr("y","0")
+      .attr("width","10")
+      .attr("height","10")
+      .append("g")
+    
+    pattern_enter.append("rect")
+      .attr("x","0")
+      .attr("y","0")
+      .attr("width","10")
+      .attr("height","10")
+      .attr("fill",color)
+      .attr("fill-opacity",0.25)
       
-      destroy("active")
-      destroy("temp")
-      
-      group.selectAll("#clip_"+d.d3plus.id)
-        .transition().delay(vars.style.timing.transitions)
-        .remove()
+    function hatch_lines(l) {
+      l
+        .attr("stroke",color)
+        .attr("stroke-width",1)
+        .attr("shape-rendering",vars.style.rendering)
     }
+    
+    pattern_enter.append("line")
+      .attr("x1","0")
+      .attr("x2","10")
+      .attr("y1","0")
+      .attr("y2","10")
+      .call(hatch_lines)
+    
+    pattern_enter.append("line")
+      .attr("x1","-1")
+      .attr("x2","1")
+      .attr("y1","9")
+      .attr("y2","11")
+      .call(hatch_lines)
+    
+    pattern_enter.append("line")
+      .attr("x1","9")
+      .attr("x2","11")
+      .attr("y1","-1")
+      .attr("y2","1")
+      .call(hatch_lines)
+    
+    var clip_data = fill_data.length ? [d] : []
+    
+    var clip = group.selectAll("#d3plus_clip_"+d.d3plus.id)
+      .data(clip_data)
+      
+    clip.enter().insert("clipPath",".d3plus_mouse")
+      .attr("id","d3plus_clip_"+d.d3plus.id)
+      .append("rect")
+        .attr("class","d3plus_clipping")
+        .call(init)
+        
+    clip.selectAll("rect").transition().duration(vars.style.timing.transitions)
+      .call(update)
+    
+    clip.exit().transition().delay(vars.style.timing.transitions)
+      .remove()
+      
+    var fills = group.selectAll("path.d3plus_fill")
+      .data(fill_data)
+      
+    fills.transition().duration(vars.style.timing.transitions)
+      .call(d3plus.shape.style,vars)
+      .call(size)
+      
+    fills.enter().insert("path","rect.d3plus_mouse")
+      .attr("class","d3plus_fill")
+      .attr("clip-path","url(#d3plus_clip_"+d.d3plus.id+")")
+      .transition().duration(0)
+        .call(size,0,undefined,0)
+        .call(d3plus.shape.style,vars)
+        .transition().duration(vars.style.timing.transitions)
+          .call(size)
+  
+    fills.exit().transition().duration(vars.style.timing.transitions)
+      .call(size,0,undefined,0)
+      .remove()
+    
   })
   
 }
