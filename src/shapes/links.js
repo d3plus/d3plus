8a6cc015d7cfb9e31c799896b64d43c6102e281b
Dave Landry <landry.dave@gmail.com>
2014-02-21 17:05:52 -0500

    implemented link label calculations

--- src/shapes/links.js
+++ src/shapes/links.js
@@ -90,48 +90,147 @@ d3plus.shape.links = function(vars,links) {
   }
   
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Calculates and Draws Label for link
+  //----------------------------------------------------------------------------
+  function label(d) {
+    
+    if (!d.d3plus) {
+      d.d3plus = {}
+    }
+      
+    delete d.d3plus_label
+  
+    if (vars.g.links.selectAll("line, path").size() < vars.links.large && vars.links.label && d[vars.links.label]) {
+      
+      if ("spline" in d.d3plus) {
+
+        var length = this.getTotalLength(),
+            center = this.getPointAtLength(length/2),
+            prev = this.getPointAtLength((length/2)-(length*.1)),
+            next = this.getPointAtLength((length/2)+(length*.1)),
+            radians = Math.atan2(next.y-prev.y,next.x-prev.x),
+            angle = radians*(180/Math.PI),
+            bounding = this.parentNode.getBBox(),
+            width = length*.8,
+            x = d.d3plus.translate.x+center.x,
+            y = d.d3plus.translate.y+center.y,
+            translate = {
+              "x": d.d3plus.translate.x+center.x,
+              "y": d.d3plus.translate.y+center.y
+            }
+            
+      }
+      else {
+
+        var bounds = this.getBBox()
+            start = {"x": d.source.d3plus.x, "y": d.source.d3plus.y},
+            end = {"x": d.target.d3plus.x, "y": d.target.d3plus.y},
+            xdiff = end.x-start.x,
+            ydiff = end.y-start.y,
+            center = {"x": end.x-(xdiff)/2, "y": end.y-(ydiff)/2},
+            radians = Math.atan2(ydiff,xdiff),
+            angle = radians*(180/Math.PI),
+            length = Math.sqrt((xdiff*xdiff)+(ydiff*ydiff)),
+            width = length-(vars.style.labels.padding*2),
+            x = center.x,
+            y = center.y,
+            translate = {
+              "x": center.x,
+              "y": center.y
+            }
+            
+      }
+      
+      if (angle < -90 || angle > 90) {
+        angle -= 180
+      }
+    
+      d.d3plus_label = {
+        "x": x,
+        "y": y,
+        "translate": translate,
+        "w": width,
+        "h": 15,
+        "angle": angle,
+        "anchor": "middle",
+        "valign": "center",
+        "color": d3plus.color.legible(vars.style.links.color),
+        "resize": false,
+        "names": [vars.format(d[vars.links.label])],
+        "background": true
+      }
+    
+    }
+    
+    d3plus.shape.labels(vars,d3.select(this.parentNode))
+        
+  }
+  
+  //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // Bind "links" data to lines in the "links" group
   //----------------------------------------------------------------------------
   var line_data = links.filter(function(l){
-    return !l.d3plus || !l.d3plus.spline
+    return !l.d3plus || (l.d3plus && !("spline" in l.d3plus))
   })
   
-  var lines = vars.g.links.selectAll("line")
+  var lines = vars.g.links.selectAll("g.d3plus_link_line")
     .data(line_data,function(d){
       return d.source[vars.id.key]+"_"+d.target[vars.id.key]
     })
+    
+  lines.selectAll("text.d3plus_label, rect.d3plus_label_bg")
+    .transition().duration(vars.style.timing.transitions/2)
+    .attr("opacity",0)
+    .remove()
   
-  lines.enter().append("line")
-    .call(line)
-    .call(init)
-  
-  lines.transition().duration(vars.style.timing.transitions)
+  lines.selectAll("line").transition().duration(vars.style.timing.transitions)
     .call(line)
     .call(style)
+    .each("end",label)
+  
+  lines.enter().append("g")
+    .attr("class","d3plus_link_line")
+    .append("line")
+    .call(line)
+    .call(init)
+    .transition().duration(vars.style.timing.transitions)
+      .call(style)
+      .each("end",label)
   
   lines.exit().transition().duration(vars.style.timing.transitions)
-    .call(init)
+    .attr("opacity",0)
     .remove()
     
   var spline_data = links.filter(function(l){
     return l.d3plus && l.d3plus.spline
   })
   
-  var splines = vars.g.links.selectAll("path")
+  var splines = vars.g.links.selectAll("g.d3plus_link_path")
     .data(spline_data,function(d){
       return d.source[vars.id.key]+"_"+d.target[vars.id.key]
     })
     
-  splines.enter().append("path")
-    .call(spline)
-    .call(init)
+  splines.selectAll("text.d3plus_label, rect.d3plus_label_bg")
+    .transition().duration(vars.style.timing.transitions/2)
+    .attr("opacity",0)
+    .remove()
   
-  splines.transition().duration(vars.style.timing.transitions)
+  splines.selectAll("path").transition().duration(vars.style.timing.transitions)
     .call(spline)
     .call(style)
+    .each("end",label)
+    
+  splines.enter().append("g")
+    .attr("class","d3plus_link_path")
+    .append("path")
+    .call(spline)
+    .call(init)
+    .transition().duration(vars.style.timing.transitions)
+      .call(style)
+      .each("end",label)
   
   splines.exit().transition().duration(vars.style.timing.transitions)
-    .call(init)
+    .attr("opacity",0)
     .remove()
   
 }
