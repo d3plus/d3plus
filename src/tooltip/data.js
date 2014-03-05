42f6ad5994cb0227551f835913039e429c9f7c3d
Dave Landry <landry.dave@gmail.com>
2014-03-05 10:25:41 -0500

    added ability to specify id depth for app tooltip data

--- src/tooltip/data.js
+++ src/tooltip/data.js
@@ -2,7 +2,7 @@
 // Creates a data object for the Tooltip
 //-------------------------------------------------------------------
 
-d3plus.tooltip.data = function(vars,id,length,extras) {
+d3plus.tooltip.data = function(vars,id,length,extras,depth) {
 
   if (!length) var length = "long"
   if (length == "long") {
@@ -11,7 +11,7 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
   else {
     var other_length = "long"
   }
-  
+
   var extra_data = {}
   if (extras && typeof extras == "string") extras = [extras]
   else if (extras && typeof extras == "object") {
@@ -22,9 +22,9 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
     }
   }
   else if (!extras) var extras = []
-  
+
   var tooltip_highlights = []
-  
+
   if (vars.tooltip.value instanceof Array) {
     var a = vars.tooltip.value
   }
@@ -32,16 +32,16 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
     var a = [vars.tooltip.value]
   }
   else {
-    
-    if (vars.tooltip.value[vars.id.nesting[vars.depth.value]]) {
-      var a = vars.tooltip.value[vars.id.nesting[vars.depth.value]]
+
+    if (vars.tooltip.value[vars.id.nesting[depth]]) {
+      var a = vars.tooltip.value[vars.id.nesting[depth]]
     }
     else {
       var a = vars.tooltip.value
     }
-    
+
     if (!(a instanceof Array)) {
-      
+
       if (a[length]) {
         a = a[length]
       }
@@ -51,66 +51,66 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
       else {
         a = d3plus.utils.merge({"":[]},a)
       }
-      
+
     }
-    
+
     if (typeof a == "string") {
       a = [a]
     }
     else if (!(a instanceof Array)) {
       a = d3plus.utils.merge({"":[]},a)
     }
-    
+
   }
-  
+
   function format_key(key,group) {
     if (vars.attrs.value[group]) var id_var = group
     else var id_var = null
-    
+
     if (group) group = vars.format(group)
-    
+
     var value = extra_data[key] || d3plus.variable.value(vars,id,key,id_var)
-    
+
     if (value !== false && value !== null) {
       var name = vars.format(key),
           h = tooltip_highlights.indexOf(key) >= 0
-          
+
       var val = vars.format(value,key)
-      
+
       var obj = {"name": name, "value": val, "highlight": h, "group": group}
-      
+
       if (vars.descs[key]) obj.desc = vars.descs[key]
-    
+
       if (val) tooltip_data.push(obj)
     }
-    
+
   }
-     
+
   var tooltip_data = []
   if (a instanceof Array) {
-    
+
     extras.forEach(function(e){
       if (a.indexOf(e) < 0) a.push(e)
     })
-    
+
     a.forEach(function(t){
       format_key(t)
     })
-  
+
   }
   else {
-    
-    if (vars.id.nesting.length && vars.depth.value < vars.id.nesting.length-1) {
+
+    if (vars.id.nesting.length && depth < vars.id.nesting.length-1) {
       var a = d3plus.utils.copy(a)
       vars.id.nesting.forEach(function(n,i){
-        if (i > vars.depth.value && a[n]) delete a[n]
+        if (i > depth && a[n]) delete a[n]
       })
     }
-    
+
     if (vars.tooltip.value.long && typeof vars.tooltip.value.long == "object") {
       var placed = []
       for (group in vars.tooltip.value.long) {
-        
+
         extras.forEach(function(e){
           if (vars.tooltip.value.long[group].indexOf(e) >= 0 && ((a[group] && a[group].indexOf(e) < 0) || !a[group])) {
             if (!a[group]) a[group] = []
@@ -150,14 +150,14 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
         })
       }
     }
-    
+
     if (a[""]) {
       a[""].forEach(function(t){
         format_key(t,"")
       })
       delete a[""]
     }
-    
+
     for (group in a) {
       if (a[group] instanceof Array) {
         a[group].forEach(function(t){
@@ -168,9 +168,9 @@ d3plus.tooltip.data = function(vars,id,length,extras) {
         format_key(a[group],group)
       }
     }
-    
+
   }
-  
+
   return tooltip_data
-  
+
 }
