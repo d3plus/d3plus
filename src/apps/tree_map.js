8b9e3d4e8d87f78fbbc9b695066cd9777b7dd9cd
Dave Landry <landry.dave@gmail.com>
2014-03-07 13:49:10 -0500

    moved grouping function inside of tree_map

--- src/apps/tree_map.js
+++ src/apps/tree_map.js
@@ -1,30 +1,40 @@
 d3plus.apps.tree_map = {}
-d3plus.apps.tree_map.data = "nested";
+d3plus.apps.tree_map.data = "grouped";
 d3plus.apps.tree_map.requirements = ["size"];
 d3plus.apps.tree_map.tooltip = "follow"
 d3plus.apps.tree_map.shapes = ["square"];
 
 d3plus.apps.tree_map.draw = function(vars) {
-  
+
+  var grouped_data = d3.nest()
+
+  vars.id.nesting.forEach(function(n,i){
+    if (i < vars.depth.value) {
+      grouped_data.key(function(d){return d[n]})
+    }
+  })
+
+  grouped_data = grouped_data.entries(vars.data.app)
+
   var data = d3.layout.treemap()
-    .round(false)
+    .round(true)
     .size([vars.app_width, vars.app_height])
-    .children(function(d) { return d.children; })
+    .children(function(d) { return d.values; })
     .padding(1)
     .sort(function(a, b) { return a.value - b.value; })
     .value(function(d) { return d3plus.variable.value(vars,d,vars.size.key); })
-    .nodes({"name":"root", "children": vars.data.app})
+    .nodes({"name":"root", "values": grouped_data})
     .filter(function(d) {
-      return !d.children && d.area;
+      return !d.values && d.area;
     })
-  
+
   if (data.length) {
-  
+
     var root = data[0]
     while (root.parent) {
       root = root.parent
     }
-  
+
     data.forEach(function(d){
       d.d3plus.x = d.x+d.dx/2
       d.d3plus.y = d.y+d.dy/2
@@ -32,9 +42,9 @@ d3plus.apps.tree_map.draw = function(vars) {
       d.d3plus.height = d.dy
       d.d3plus.share = d.value/root.value
     })
-  
+
   }
-  
+
   return data
-  
+
 }
