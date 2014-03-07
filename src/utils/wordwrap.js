9847640babd4ac694166b11324f10d53bfac703b
Dave Landry <landry.dave@gmail.com>
2014-03-07 13:54:46 -0500

    catch for when padding word-wrap a single string and not an array of strings

--- src/utils/wordwrap.js
+++ src/utils/wordwrap.js
@@ -1,40 +1,40 @@
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // function that will wrap and resize SVG text
 //-------------------------------------------------------------------
-
 d3plus.utils.wordwrap = function(params) {
-  
+
   var parent = params.parent,
       width = params.width ? params.width : 20000,
       height = params.height ? params.height : 20000,
       resize = params.resize,
       font_max = params.font_max ? params.font_max : 40,
       font_min = params.font_min ? params.font_min : 9,
-      text_array = params.text.slice(0),
+      text_array = params.text,
       split = ["-","/",";",":","%","&"],
       regex = new RegExp("[^\\s\\"+split.join("\\")+"]+\\"+split.join("?\\")+"?","g"),
       current_text = ""
-      
+
   if (text_array instanceof Array) {
+    text_array = text_array.slice(0)
     current_text = String(text_array.shift())
   }
   else {
     current_text = String(text_array)
   }
-  
+
   wrap()
-  
+
   function wrap() {
-    
+
     var words = current_text.match(regex)
-    
+
     if (resize) {
-      
+
       // Start by trying the largest font size
       var size = font_max
       size = Math.floor(size)
       d3.select(parent).attr("font-size",size)
-    
+
       // Add each word to it's own line (to test for longest word)
       d3.select(parent).selectAll("tspan").remove()
       for(var i=0; i<words.length; i++) {
@@ -42,10 +42,10 @@ d3plus.utils.wordwrap = function(params) {
         else var t = words[i]+"..."
         d3.select(parent).append("tspan").attr("x",0).text(t)
       }
-    
+
       // If the longest word is too wide, make the text proportionately smaller
       if (parent.getBBox().width > width) size = size*(width/parent.getBBox().width)
-      
+
       // If the new size is too small, return NOTHING
       if (size < font_min) {
         d3.select(parent).selectAll("tspan").remove();
@@ -60,10 +60,10 @@ d3plus.utils.wordwrap = function(params) {
       // Use new text size
       size = Math.floor(size)
       d3.select(parent).attr("font-size",size);
-    
+
       // Flow text into box
       flow();
-      
+
       // If text doesn't fit height-wise, shrink it!
       if (parent.childNodes.length*parent.getBBox().height > height) {
         var temp_size = size*(height/(parent.childNodes.length*parent.getBBox().height))
@@ -72,45 +72,45 @@ d3plus.utils.wordwrap = function(params) {
         size = Math.floor(size)
         d3.select(parent).attr("font-size",size)
       } else finish();
-    
+
     }
-  
+
     flow();
     truncate();
     finish();
-  
+
     function flow() {
-    
+
       d3.select(parent).selectAll("tspan").remove()
-      
+
       var x_pos = parent.getAttribute("x")
-      
+
       var tspan = d3.select(parent).append("tspan")
         .attr("x",x_pos)
         .text(words[0])
 
       for (var i=1; i < words.length; i++) {
-        
+
         var current = tspan.text(),
             last_char = current.slice(-1),
             next_char = current_text.charAt(current_text.indexOf(current)+current.length)
             joiner = next_char == " " ? " " : ""
-            
+
         tspan.text(current+joiner+words[i])
-      
+
         if (tspan.node().getComputedTextLength() > width) {
-            
+
           tspan.text(current)
-    
+
           tspan = d3.select(parent).append("tspan")
             .attr("x",x_pos)
             .text(words[i])
-            
+
         }
       }
-  
+
     }
-  
+
     function truncate() {
       var cut = false
       while (parent.childNodes.length*parent.getBBox().height > height && parent.lastChild && !cut) {
@@ -120,12 +120,12 @@ d3plus.utils.wordwrap = function(params) {
       if (cut) {
         tspan = parent.lastChild
         words = d3.select(tspan).text().match(/[^\s-]+-?/g)
-      
+
         var last_char = words[words.length-1].charAt(words[words.length-1].length-1)
         if (last_char == "," || last_char == ";" || last_char == ":") words[words.length-1] = words[words.length-1].substr(0,words[words.length-1].length-1)
-      
+
         d3.select(tspan).text(words.join(" ")+"...")
-      
+
         if (tspan.getComputedTextLength() > width) {
           if (words.length > 1) words.pop(words.length-1)
           last_char = words[words.length-1].charAt(words[words.length-1].length-1)
@@ -135,10 +135,10 @@ d3plus.utils.wordwrap = function(params) {
       }
     }
   }
-  
+
   function finish() {
     d3.select(parent).selectAll("tspan").attr("dy", d3.select(parent).style("font-size"));
     return;
   }
-  
+
 }
