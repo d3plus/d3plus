cd09c94bf9f83ee8d414681bc89541c426d89583
Dave Landry <landry.dave@gmail.com>
2014-03-10 17:22:45 -0400

    forms now support superfluous data- attributes

--- src/forms/data.js
+++ src/forms/data.js
@@ -1,35 +1,38 @@
 d3plus.forms.data = function(vars) {
-  
+
   if (vars.data.data) {
 
     if (!vars.data.array || (("replace" in vars.data && vars.data.replace === true) || !("replace" in vars.data))) {
       vars.data.array = []
     }
-    
-    var vals = ["value","alt","keywords","image","style","color","selected","text"],
-        map = vars.data.map || {}
-        
+
+    var defaults = ["value","alt","keywords","image","style","color","selected","text"],
+        vals = vars.data.map || {}
+
+    defaults.forEach(function(d){
+      if (!(d in vals)) {
+        vals[d] = d
+      }
+    })
+
     vars.data.data.forEach(function(d){
       var obj = {}
       for (key in vals) {
-        if (typeof map[vals[key]] == "string" && map[vals[key]] in d) {
-          obj[vals[key]] = d[map[vals[key]]]
-        }
-        else if (key in d) {
-          obj[vals[key]] = d[vals[key]]
+        if (vals[key] in d) {
+          obj[key] = d[vals[key]]
         }
       }
       vars.data.array.push(obj)
     })
-    
+
     var sort = "sort" in vars.data ? vars.data.sort : "text"
     if (sort) {
-      
+
       vars.data.array.sort(function(a,b){
-        
+
         a = a[sort]
         b = b[sort]
-        
+
         if (sort == "color") {
 
           a = d3.rgb(a_value).hsl()
@@ -39,19 +42,19 @@ d3plus.forms.data = function(vars) {
           else a = a.h
           if (b.s == 0) b = 361
           else b = b.h
-        
+
         }
-            
+
         if(a < b) return -1;
         if(a > b) return 1;
-        
+
       })
-      
+
     }
-    
+
   }
-  
+
   vars.data.changed = true
   vars.loading = false
-  
+
 }
