a79c2594a1e4083ae0e51bb5af20876261312723
Dave Landry <landry.dave@gmail.com>
2014-03-12 12:28:32 -0400

    fixed focus tooltip

--- src/ui/focus.js
+++ src/ui/focus.js
@@ -4,33 +4,39 @@
 
 d3plus.ui.focus = function(vars) {
 
-  if (vars.focus.value) {
+  if (!vars.internal_error && vars.focus.value) {
+
     var data = vars.data.pool.filter(function(d){
       return d3plus.variable.value(vars,d,vars.id.key) == vars.focus.value
     })
+
+    if (data.length >= 1) {
+      data = data[0]
+    }
+    else {
+      data = {}
+      data[vars.id.key] = vars.focus.value
+    }
+
+    d3plus.tooltip.app({
+      "anchor": "top right",
+      "arrow": false,
+      "data": data,
+      "length": "long",
+      "fullscreen": false,
+      "id": vars.type.value+"_focus",
+      "offset": 0,
+      "vars": vars,
+      "x": vars.width.value-vars.margin.right,
+      "y": vars.margin.top,
+      "width": vars.style.tooltip.width
+    })
+
+    vars.app_width -= vars.style.tooltip.width
+
   }
-
-  if (!vars.internal_error && data && data.length == 1) {
-    console.log(data)
-    // var d = vars.focus.tooltip.value,
-    //     d = d.data ? d.data : d,
-    //     depth = d.d3plus && "depth" in d.d3plus ? d.d3plus.depth : vars.depth.value;
-    //
-    // d3plus.tooltip.app({
-    //   "anchor": "top right",
-    //   "arrow": false,
-    //   "data": d,
-    //   "depth": depth,
-    //   "length": "long",
-    //   "fullscreen": false,
-    //   "id": vars.type.value+"_focus",
-    //   "vars": vars,
-    //   "x": vars.width.value-vars.margin.right,
-    //   "y": vars.margin.top
-    // })
-    //
-    // vars.app_width -= vars.style.tooltip.width
-
+  else {
+    d3plus.tooltip.remove(vars.type.value+"_focus")
   }
 
 }
