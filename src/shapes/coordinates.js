bad918e03275956454c6b6ae444512ec2099692d
Dave Landry <landry.dave@gmail.com>
2014-03-13 15:02:48 -0400

    fixed coordinate bounds

--- src/shapes/coordinates.js
+++ src/shapes/coordinates.js
@@ -7,9 +7,10 @@ d3plus.shape.coordinates = function(vars,selection,enter,exit) {
   // Define the geographical projection
   //----------------------------------------------------------------------------
   var projection = d3.geo[vars.coords.projection.value]()
+    // .translate([-vars.app_width/2,-vars.app_height/2])
 
-  var clip = d3.geo.clipExtent()
-      .extent([[0, 0], [vars.app_width, vars.app_height]]);
+  // var clip = d3.geo.clipExtent()
+  //     .extent([[0, 0], [vars.app_width, vars.app_height]]);
 
   if (!vars.zoom.scale) {
     vars.zoom.scale = 1
@@ -66,35 +67,46 @@ d3plus.shape.coordinates = function(vars,selection,enter,exit) {
       .call(d3plus.shape.style,vars)
   }
 
-  if (vars.coords.changed || vars.width.changed || vars.height.changed) {
-    if (vars.viewport == vars.bounds) {
-      vars.viewport = false
-    }
-    vars.bounds = false
-    selection.each(function(d){
-      var b = path.bounds(d)
-      if (!vars.bounds) {
-        vars.bounds = b
-      }
-      else {
-        if (vars.bounds[0][0] > b[0][0]) {
-          vars.bounds[0][0] = b[0][0]
-        }
-        if (vars.bounds[0][1] > b[0][1]) {
-          vars.bounds[0][1] = b[0][1]
-        }
-        if (vars.bounds[1][0] < b[1][0]) {
-          vars.bounds[1][0] = b[1][0]
-        }
-        if (vars.bounds[1][1] < b[1][1]) {
-          vars.bounds[1][1] = b[1][1]
-        }
-      }
-    })
-  }
+  if (vars.coords.changed || vars.width.changed || vars.height.changed || (vars.focus.value && vars.focus.changed)) {
 
-  if (!vars.viewport) {
-    d3plus.zoom.reset(vars)
+    if (!vars.focus.value) {
+      vars.zoom.bounds = false
+    }
+    selection.each(function(d){
+
+      if (vars.focus.value && d[vars.id.key] == vars.focus.value) {
+        vars.zoom.viewport = path.bounds(d)
+      }
+
+      if (!vars.focus.value) {
+        var b = path.bounds(d)
+        if (!vars.zoom.bounds) {
+          vars.zoom.bounds =  b
+        }
+        else {
+          if (vars.zoom.bounds[0][0] > b[0][0]) {
+            vars.zoom.bounds[0][0] = b[0][0]
+          }
+          if (vars.zoom.bounds[0][1] > b[0][1]) {
+            vars.zoom.bounds[0][1] = b[0][1]
+          }
+          if (vars.zoom.bounds[1][0] < b[1][0]) {
+            vars.zoom.bounds[1][0] = b[1][0]
+          }
+          if (vars.zoom.bounds[1][1] < b[1][1]) {
+            vars.zoom.bounds[1][1] = b[1][1]
+          }
+        }
+
+      }
+
+    })
+
+  }
+  else if (vars.focus.changed) {
+    if (!vars.focus.value) {
+      vars.zoom.viewport = vars.zoom.bounds
+    }
   }
 
 }
