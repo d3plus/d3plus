9e39f6deac97e19c50e34bddac7d4dcd4bfd2623
Dave Landry <landry.dave@gmail.com>
2014-03-13 15:04:12 -0400

    zoom mouse and bounds function now work

--- src/zoom/bounds.js
+++ src/zoom/bounds.js
@@ -1,29 +1,55 @@
 d3plus.zoom.bounds = function(vars,b) {
-  
-  vars.viewport = b
-  
-  if (vars.coords.fit.value == "auto") {
-    var aspect = Math.max((b[1][0]-b[0][0])/vars.app_width, (b[1][1]-b[0][1])/vars.app_height)
+
+  if (!b) {
+    var b = vars.zoom.bounds
+  }
+
+  vars.zoom.size = {
+    "height": b[1][1]-b[0][1],
+    "width": b[1][0]-b[0][0]
+  }
+
+  var fit = vars.coords.fit.value
+  if (fit == "auto") {
+    var aspect = d3.max([vars.zoom.size.width/vars.app_width,vars.zoom.size.height/vars.app_height])
   }
   else {
-    var w = vars.coords.fit.value == "width" ? b[1][0]-b[0][0] : b[1][1]-b[0][1],
-        aspect = w/vars["app_"+vars.coords.fit.value]
+    var aspect = vars.zoom.size[fit]/vars["app_"+fit]
   }
-  
+
   var min = d3.min([vars.app_width,vars.app_height])
-  
-  vars.zoom.scale = ((min-(vars.coords.padding*2)) / min) / aspect
-  
-  var translate = [
-    -(b[1][0] + b[0][0]) / 2,
-    -(b[1][1] + b[0][1]) / 2
-  ]
-  
+
+  var scale = ((min-(vars.coords.padding*2)) / min) / aspect,
+      max_scale = vars.zoom_behavior.scaleExtent()[1]
+  if (scale > max_scale) {
+    scale = max_scale
+  }
+  vars.zoom.scale = scale
+
+  var translate = []
+
+  translate[0] = vars.app_width/2-(vars.zoom.size.width*scale)/2-(b[0][0]*scale)
+  translate[1] = vars.app_height/2-(vars.zoom.size.height*scale)/2-(b[0][1]*scale)
+
   vars.zoom.translate = translate
-      
-  vars.g.zoom
-    .attr("transform","translate(" + [vars.app_width/2,vars.app_height/2] + ")"
-      + "scale(" + vars.zoom.scale + ")"
-      + "translate(" + vars.zoom.translate[0] + "," + vars.zoom.translate[1] + ")")
-      
+  vars.zoom_behavior.translate(translate).scale(scale)
+
+  if (!vars.zoom.viewport) {
+    vars.zoom_behavior.scaleExtent([scale,scale*8])
+  }
+
+  vars.zoom.viewport = b
+  vars.zoom.size = {
+    "height": vars.zoom.bounds[1][1]-vars.zoom.bounds[0][1],
+    "width": vars.zoom.bounds[1][0]-vars.zoom.bounds[0][0]
+  }
+
+  if (vars.timing) {
+    vars.g.viz.transition().duration(vars.timing)
+      .attr("transform","translate("+translate+")scale("+scale+")")
+  }
+  else {
+    vars.g.viz.attr("transform","translate("+translate+")scale("+scale+")")
+  }
+
 }
--- src/zoom/mouse.js
+++ src/zoom/mouse.js
@@ -1,25 +1,41 @@
 d3plus.zoom.mouse = function(vars) {
 
   var translate = d3.event.translate,
-      scale = d3.event.scale
-      
-  // console.log(translate)
+      scale = d3.event.scale,
+      limits = vars.zoom.bounds,
+      xoffset = (vars.app_width-(vars.zoom.size.width*scale))/2,
+      xmin = xoffset > 0 ? xoffset : 0,
+      xmax = xoffset > 0 ? vars.app_width-xoffset : vars.app_width,
+      yoffset = (vars.app_height-(vars.zoom.size.height*scale))/2,
+      ymin = yoffset > 0 ? yoffset : 0,
+      ymax = yoffset > 0 ? vars.app_height-yoffset : vars.app_height
+
   // Auto center visualization
-  if (translate[0] > 0) {
-    translate[0] = 0
+  if (translate[0]+limits[0][0]*scale > xmin) {
+    translate[0] = -limits[0][0]*scale+xmin
   }
-  else if (translate[0] < -((vars.app_width*scale)-vars.app_width)) {
-    translate[0] = -((vars.app_width*scale)-vars.app_width)
+  else if (translate[0]+limits[1][0]*scale < xmax) {
+    translate[0] = xmax-(limits[1][0]*scale)
   }
-  if (translate[1] > 0) {
-    translate[1] = 0
+
+  if (translate[1]+limits[0][1]*scale > ymin) {
+    translate[1] = -limits[0][1]*scale+ymin
   }
-  else if (translate[1] < -((vars.app_height*scale)-vars.app_height)) {
-    translate[1] = -((vars.app_height*scale)-vars.app_height)
+  else if (translate[1]+limits[1][1]*scale < ymax) {
+    translate[1] = ymax-(limits[1][1]*scale)
   }
 
   vars.zoom_behavior.translate(translate).scale(scale)
 
-  vars.g.viz.attr("transform","translate("+translate+")scale("+scale+")")
+  vars.zoom.translate = translate
+  vars.zoom.scale = scale
+
+  if (d3.event.sourceEvent.type == "dblclick") {
+    vars.g.viz.transition().duration(vars.timing)
+      .attr("transform","translate("+translate+")scale("+scale+")")
+  }
+  else {
+    vars.g.viz.attr("transform","translate("+translate+")scale("+scale+")")
+  }
 
 }
