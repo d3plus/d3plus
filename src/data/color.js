e9993459cdcefc6832ec0445ec70765afdd5dd73
Dave Landry <landry.dave@gmail.com>
2014-03-13 15:01:23 -0400

    fix for color scale disappearing on update

--- src/data/color.js
+++ src/data/color.js
@@ -1,9 +1,8 @@
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Sets color range of data, if applicable
 //-------------------------------------------------------------------
-
 d3plus.data.color = function(vars) {
-  
+
   if (vars.color.key && typeof vars.color.key == "object") {
     if (vars.color.key[vars.id.key]) {
       var color_id = vars.color.key[vars.id.key]
@@ -15,29 +14,29 @@ d3plus.data.color = function(vars) {
   else {
     var color_id = vars.color.key
   }
-  
+
   if (vars.data.value && color_id && vars.data.keys[color_id] == "number" && (vars.color.changed || vars.data.changed || vars.depth.changed || vars.time.solo.changed || vars.time.mute.changed)) {
 
     if (vars.dev.value) d3plus.console.group("Calculating Color Scale")
 
     if (vars.dev.value) d3plus.console.time("get data range")
-  
+
     var data_range = []
     vars.data.pool.forEach(function(d){
       var val = parseFloat(d3plus.variable.value(vars,d,color_id))
       if (typeof val == "number" && !isNaN(val) && data_range.indexOf(val) < 0) data_range.push(val)
     })
-    
+
     if (vars.dev.value) d3plus.console.timeEnd("get data range")
-    
+
     if (data_range.length > 1) {
-    
+
       var data_domain = null
-    
+
       if (vars.dev.value) d3plus.console.time("create color scale")
-      
+
       data_range = d3.extent(data_range)
-    
+
       if (data_range[0] < 0 && data_range[1] > 0) {
         var color_range = vars.style.color.range
         data_range.push(data_range[1])
@@ -50,24 +49,24 @@ d3plus.data.color = function(vars) {
       else {
         var color_range = vars.style.color.range.slice(0)
       }
-      
+
       vars.color_scale = d3.scale.sqrt()
         .domain(data_range)
         .range(color_range)
         .interpolate(d3.interpolateRgb)
-  
+
       if (vars.dev.value) d3plus.console.timeEnd("create color scale")
-      
+
     }
     else {
       vars.color_scale = null
     }
-    
+
     if (vars.dev.value) d3plus.console.groupEnd()
-    
+
   }
-  else {
+  else if (!color_id || (color_id && vars.data.keys[color_id] != "number")) {
     vars.color_scale = null
   }
-  
+
 }
