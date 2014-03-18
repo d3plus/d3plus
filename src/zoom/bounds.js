c68e8a0adf86d25c49e155918ec706d810a13283
Dave Landry <landry.dave@gmail.com>
2014-03-17 21:56:51 -0400

    moved zoom transform to separate file

--- src/zoom/bounds.js
+++ src/zoom/bounds.js
@@ -14,7 +14,7 @@ d3plus.zoom.bounds = function(vars,b,timing) {
   }
 
   var fit = vars.coords.fit.value
-  if (fit == "auto") {
+  if (fit == "auto" || d3plus.apps[vars.type.value].requirements.indexOf("coords") < 0) {
     var aspect = d3.max([vars.zoom.size.width/vars.app_width,vars.zoom.size.height/vars.app_height])
   }
   else {
@@ -23,8 +23,13 @@ d3plus.zoom.bounds = function(vars,b,timing) {
 
   var min = d3.min([vars.app_width,vars.app_height])
 
-  var scale = ((min-(vars.coords.padding*2)) / min) / aspect,
-      max_scale = vars.zoom_behavior.scaleExtent()[1]
+  var scale = ((min-(vars.coords.padding*2)) / min) / aspect
+  
+  if (!vars.zoom.viewport) {
+    vars.zoom_behavior.scaleExtent([scale,scale*8])
+  }
+
+  var max_scale = vars.zoom_behavior.scaleExtent()[1]
   if (scale > max_scale) {
     scale = max_scale
   }
@@ -38,28 +43,12 @@ d3plus.zoom.bounds = function(vars,b,timing) {
   vars.zoom.translate = translate
   vars.zoom_behavior.translate(translate).scale(scale)
 
-  if (!vars.zoom.viewport) {
-    vars.zoom_behavior.scaleExtent([scale,scale*8])
-  }
-
   vars.zoom.viewport = b
   vars.zoom.size = {
     "height": vars.zoom.bounds[1][1]-vars.zoom.bounds[0][1],
     "width": vars.zoom.bounds[1][0]-vars.zoom.bounds[0][0]
   }
-  
-  if (timing) {
-    vars.g.viz.transition().duration(timing)
-      .attr("transform","translate("+translate+")scale("+scale+")")
 
-    vars.defs.selectAll("marker").selectAll("path")
-      .transition().duration(timing)
-      .attr("transform","scale("+1/scale+")")
-
-  }
-  else {
-    vars.g.viz.attr("transform","translate("+translate+")scale("+scale+")")
-    vars.defs.selectAll("marker").selectAll("path").attr("transform","scale("+1/scale+")")
-  }
+  d3plus.zoom.transform(vars,timing)
 
 }
