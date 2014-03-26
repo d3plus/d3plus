57dc214270a26193727ccf4ce71df9bc6ae18adb
Dave Landry <landry.dave@gmail.com>
2014-03-26 10:50:30 -0400

    sped up drawing rectangles for large datasets

--- src/shapes/rect.js
+++ src/shapes/rect.js
@@ -4,6 +4,52 @@
 d3plus.shape.rect = function(vars,selection,enter,exit) {
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Calculate label position and pass data from parent.
+  //----------------------------------------------------------------------------
+  function data(d) {
+
+    if (vars.labels.value && !d.d3plus.label) {
+
+      d.d3plus_label = {
+        "w": 0,
+        "h": 0,
+        "x": 0,
+        "y": 0
+      }
+
+      var w = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.width,
+          h = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.height
+
+      // Square bounds
+      if (vars.shape.value == "square") {
+
+        d.d3plus_share = {
+          "w": w,
+          "h": h/4,
+          "x": 0,
+          "y": 0
+        }
+
+        d.d3plus_label.w = w
+        d.d3plus_label.h = h
+
+      }
+      // Circle bounds
+      else {
+        d.d3plus_label.w = Math.sqrt(Math.pow(w,2)*.7)
+        d.d3plus_label.h = Math.sqrt(Math.pow(h,2)*.7)
+      }
+
+    }
+    else if (d.d3plus.label) {
+      d.d3plus_label = d.d3plus.label
+    }
+
+    return [d];
+
+  }
+
+  //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // The position and size of each rectangle on enter and exit.
   //----------------------------------------------------------------------------
   function init(nodes) {
@@ -19,34 +65,34 @@ d3plus.shape.rect = function(vars,selection,enter,exit) {
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // The position and size of each rectangle on update.
   //----------------------------------------------------------------------------
-  function update(nodes,mod) {
-    if (!mod) var mod = 0
+  function update(nodes) {
+
     nodes
       .attr("x",function(d){
         var w = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.width
-        return (-w/2)-(mod/2)
+        return -w/2
       })
       .attr("y",function(d){
         var h = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.height
-        return (-h/2)-(mod/2)
+        return -h/2
       })
       .attr("width",function(d){
         var w = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.width
-        return w+mod
+        return w
       })
       .attr("height",function(d){
         var h = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.height
-        return h+mod
+        return h
       })
       .attr("rx",function(d){
         var rounded = vars.shape.value == "circle"
         var w = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.width
-        return rounded ? (w+mod+2)/2 : 0
+        return rounded ? (w+2)/2 : 0
       })
       .attr("ry",function(d){
         var rounded = vars.shape.value == "circle"
         var h = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.height
-        return rounded ? (h+mod+2)/2 : 0
+        return rounded ? (h+2)/2 : 0
       })
       .attr("transform",function(d){
         if ("rotate" in d.d3plus) {
@@ -62,70 +108,36 @@ d3plus.shape.rect = function(vars,selection,enter,exit) {
           return "auto"
         }
       })
+
   }
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // "rects" Enter
   //----------------------------------------------------------------------------
-  enter.append("rect")
-    .attr("class","d3plus_data")
-    .call(init)
-    .call(d3plus.shape.style,vars)
+  if (vars.timing) {
+    enter.append("rect")
+      .attr("class","d3plus_data")
+      .call(init)
+      .call(d3plus.shape.style,vars)
+  }
+  else {
+    enter.append("rect")
+      .attr("class","d3plus_data")
+  }
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // "rects" Update
   //----------------------------------------------------------------------------
-  selection.selectAll("rect.d3plus_data")
-    .data(function(d) {
-
-      if (vars.labels.value && !d.d3plus.label) {
-
-        d.d3plus_label = {
-          "w": 0,
-          "h": 0,
-          "x": 0,
-          "y": 0
-        }
-
-        var w = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.width,
-            h = d.d3plus.r ? d.d3plus.r*2 : d.d3plus.height
-
-        // Square bounds
-        if (vars.shape.value == "square") {
-
-          d.d3plus_share = {
-            "w": w,
-            "h": h/4,
-            "x": 0,
-            "y": 0
-          }
-
-          d.d3plus_label.w = w
-          d.d3plus_label.h = h
-
-        }
-        // Circle bounds
-        else {
-          d.d3plus_label.w = Math.sqrt(Math.pow(w,2)*.75)
-          d.d3plus_label.h = Math.sqrt(Math.pow(h,2)*.75)
-        }
-
-      }
-      else if (d.d3plus.label) {
-        d.d3plus_label = d.d3plus.label
-      }
-
-      return [d];
-    })
-
   if (vars.timing) {
     selection.selectAll("rect.d3plus_data")
+      .data(data)
       .transition().duration(vars.timing)
         .call(update)
         .call(d3plus.shape.style,vars)
   }
   else {
     selection.selectAll("rect.d3plus_data")
+      .data(data)
       .call(update)
       .call(d3plus.shape.style,vars)
   }
@@ -136,10 +148,6 @@ d3plus.shape.rect = function(vars,selection,enter,exit) {
   if (vars.timing) {
     exit.selectAll("rect.d3plus_data")
       .transition().duration(vars.timing)
-      .call(init)
-  }
-  else {
-    exit.selectAll("rect.d3plus_data")
       .call(init)
   }
 
