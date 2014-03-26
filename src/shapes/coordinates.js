3f72286eceea99cb2ba914fc8026bea46f9244df
Dave Landry <landry.dave@gmail.com>
2014-03-26 11:24:51 -0400

    wrote some preliminary code for labeling coordinates

--- src/shapes/coordinates.js
+++ src/shapes/coordinates.js
@@ -72,6 +72,30 @@ d3plus.shape.coordinates = function(vars,selection,enter,exit) {
     selection.each(function(d){
 
       var b = vars.path.bounds(d)
+
+      // if (!("d3plus_label" in d)) {
+      //   var center = vars.path.centroid(d),
+      //       largest = 0,
+      //       lb = b
+      //   d.geometry.coordinates.forEach(function(c,i){
+      //     var test = d3plus.utils.copy(d)
+      //     test.geometry.coordinates = [test.geometry.coordinates[i]]
+      //     var area = vars.path.area(test)
+      //     if (area > largest) {
+      //       lb = vars.path.bounds(test)
+      //     }
+      //   })
+      //   d.d3plus_label = {
+      //     "anchor": "middle",
+      //     "h": (lb[1][1]-lb[0][1])*.5,
+      //     "w": (lb[1][0]-lb[0][0])*.5,
+      //     "resize": false,
+      //     "valign": "center",
+      //     "x": center[0],
+      //     "y": center[1]
+      //   }
+      // }
+
       if (!vars.zoom.bounds) {
         vars.zoom.bounds =  b
       }
