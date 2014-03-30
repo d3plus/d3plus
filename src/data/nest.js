b2a6de801fc8c75b6598106d252459f2b2a17568
Dave Landry <landry.dave@gmail.com>
2014-03-29 20:32:30 -0400

    fixed issue where continuous axes values were being summed up when nested

--- src/data/nest.js
+++ src/data/nest.js
@@ -2,40 +2,40 @@
 // Nests data...
 //-------------------------------------------------------------------
 d3plus.data.nest = function(vars,flat_data,levels,grouped) {
-  
+
   var nested_data = d3.nest(), group_data = [];
-        
+
   var checks = ["active","temp","total"]
-      
+
   levels.forEach(function(nest_key, i){
-    
+
     nested_data
-      .key(function(d){ 
+      .key(function(d){
         return d3plus.variable.value(vars,d,nest_key)
       })
-      
+
     vars.axes.values.forEach(function(axis){
       if (d3plus.apps[vars.type.value].requirements && d3plus.apps[vars.type.value].requirements.indexOf(axis) >= 0 && vars[axis].key && vars[axis].scale.value == "continuous") {
         nested_data
-          .key(function(d){ 
+          .key(function(d){
             return d3plus.variable.value(vars,d,vars[axis].key)
           })
       }
     })
-    
+
     if (i == levels.length-1) {
-      
+
       nested_data.rollup(function(leaves){
-        
+
         to_return = {
           "d3plus": {
             "depth": i
           }
         }
-        
+
         checks.forEach(function(c){
           var key = vars[c].key ? vars[c].key : c
-          to_return[key] = d3.sum(leaves, function(d){ 
+          to_return[key] = d3.sum(leaves, function(d){
             if (vars[c].key) {
               var a = d3plus.variable.value(vars,d,vars[c].key)
               if (typeof a != "number") {
@@ -55,9 +55,9 @@ d3plus.data.nest = function(vars,flat_data,levels,grouped) {
 
         var nest_obj = d3plus.variable.value(vars,leaves[0],nest_key)
         to_return[nest_key] = nest_obj
-        
+
         for (key in vars.data.keys) {
-          if (((levels.indexOf(nest_key) >= 0 && levels.indexOf(key) <= levels.indexOf(nest_key)) || (vars.id.nesting.indexOf(nest_key) >= 0 && vars.id.nesting.indexOf(key) <= vars.id.nesting.indexOf(nest_key))) 
+          if (((levels.indexOf(nest_key) >= 0 && levels.indexOf(key) <= levels.indexOf(nest_key)) || (vars.id.nesting.indexOf(nest_key) >= 0 && vars.id.nesting.indexOf(key) <= vars.id.nesting.indexOf(nest_key)))
             && key in leaves[0]
             && (!vars.active.key || key != vars.active.key) && key != "d3plus") {
             if (typeof vars.aggs.value[key] == "function") {
@@ -66,7 +66,7 @@ d3plus.data.nest = function(vars,flat_data,levels,grouped) {
             else if (typeof vars.aggs.value[key] == "string") {
               to_return[key] = d3[vars.aggs.value[key]](leaves, function(d){ return d[key]; })
             }
-            else if ([vars.time.key,vars.icon].indexOf(key) >= 0 || (key == nest_key && !to_return[key])) {
+            else if ([vars.time.key,vars.icon].indexOf(key) >= 0 || (key == nest_key && !to_return[key]) || (vars.x.key == key && vars.x.scale.value == "continuous") || (vars.y.key == key && vars.y.scale.value == "continuous")) {
               to_return[key] = leaves[0][key];
             }
             else if (vars.data.keys[key] === "number" && vars.id.nesting.indexOf(key) < 0) {
@@ -77,34 +77,34 @@ d3plus.data.nest = function(vars,flat_data,levels,grouped) {
             }
           }
         }
-        
+
         if (grouped) {
           group_data.push(to_return)
         }
-        
+
         return to_return
-        
+
       })
     }
-  
+
   })
-    
-  rename_key_value = function(obj) { 
-    if (obj.values && obj.values.length) { 
-      obj.children = obj.values.map(function(obj) { 
+
+  rename_key_value = function(obj) {
+    if (obj.values && obj.values.length) {
+      obj.children = obj.values.map(function(obj) {
         return rename_key_value(obj);
       })
       delete obj.values
       return obj
-    } 
-    else if(obj.values) { 
+    }
+    else if(obj.values) {
       return obj.values
     }
     else {
-      return obj; 
+      return obj;
     }
   }
-  
+
   find_keys = function(obj,depth,keys) {
     if (obj.children) {
       if (vars.data.keys[levels[depth]] == "number") {
@@ -121,7 +121,7 @@ d3plus.data.nest = function(vars,flat_data,levels,grouped) {
       })
     }
   }
-  
+
   nested_data = nested_data
     .entries(flat_data)
     .map(rename_key_value)
@@ -129,11 +129,11 @@ d3plus.data.nest = function(vars,flat_data,levels,grouped) {
       find_keys(obj,0,{})
       return obj
     })
-    
+
   if (grouped) {
     return group_data
   }
-    
+
   return nested_data;
 
 }
