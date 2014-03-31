192f797c12a0af88e1720df197d57bff9678a1c2
Dave Landry <landry.dave@gmail.com>
2014-03-31 12:27:38 -0400

    decreased delay for detecting scroll wheel stops

--- src/zoom/mouse.js
+++ src/zoom/mouse.js
@@ -31,7 +31,7 @@ d3plus.zoom.mouse = function(vars) {
   vars.zoom.scale = scale
 
   if (d3.event.sourceEvent.type == "wheel") {
-    var delay = vars.timing ? 250 : 500
+    var delay = vars.timing ? 100 : 500
     clearTimeout(vars.zoom.wheel)
     vars.zoom.wheel = setTimeout(function(){
       d3plus.zoom.labels(vars)
