c46d4dde7b23b2e6ad10a99b45f62879484cff92
Dave Landry <landry.dave@gmail.com>
2014-04-08 10:31:37 -0400

    fixed error with new distances util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -106,11 +106,11 @@ d3plus.utils.distances = function(arr,accessor) {
 
   var distances = [], checked = []
   arr.forEach(function(node1){
-    var n1 = accessor ? accessor(node1) : [n1.x,n1.y]
+    var n1 = accessor ? accessor(node1) : [node1.x,node1.y]
     checked.push(node1)
     arr.forEach(function(node2){
       if (checked.indexOf(node2) < 0) {
-        var n2 = accessor ? accessor(node2) : [n1.x,n1.y]
+        var n2 = accessor ? accessor(node2) : [node2.x,node2.y]
           , xx = Math.abs(n1[0]-n2[0])
           , yy = Math.abs(n1[1]-n2[1])
         distances.push(Math.sqrt((xx*xx)+(yy*yy)))
