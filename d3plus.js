4af83aa062280bbd28b30bfe877e8ab2a4a59f69
Dave Landry <landry.dave@gmail.com>
2014-04-08 20:15:46 -0400

    re-compiled v1.2.2

--- d3plus.js
+++ d3plus.js
@@ -3657,7 +3657,7 @@ d3plus.apps.rings.draw = function(vars) {
     if (domain[0] == domain[1]) {
       domain[0] = 0
     }
-
+    
     var radius = d3.scale.linear()
       .domain(domain)
       .range([3,d3.min([primaryMax,secondaryMax])])
@@ -3681,7 +3681,7 @@ d3plus.apps.rings.draw = function(vars) {
   primaries.forEach(function(p,i){
 
     p.d3plus.ring = 1
-    var val = vars.size.key ? d3plus.variable.value(vars,s,vars.size.key) : 1
+    var val = vars.size.key ? d3plus.variable.value(vars,p,vars.size.key) : 1
     p.d3plus.r = radius(val)
 
     var check = [vars.edges.source,vars.edges.target]
