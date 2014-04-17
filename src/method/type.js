f935bb767cf61f353e1c25c53537fce650b68165
Dave Landry <landry.dave@gmail.com>
2014-04-17 10:43:29 -0400

    added accepted visualizations to .type() method

--- src/method/type.js
+++ src/method/type.js
@@ -1,4 +1,13 @@
 d3plus.method.type = {
+  "accepted": [ "bubbles"
+              , "chart"
+              , "geo_map"
+              , "line"
+              , "network"
+              , "rings"
+              , "scatter"
+              , "stacked"
+              , "tree_map" ],
   "mode": {
     "accepted": ["squarify","slice","dice","slice-dice"],
     "value": "squarify"
