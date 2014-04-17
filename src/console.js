681e9c39d574ff32b8f7e2ca8dfeba7aa1f42b36
Dave Landry <landry.dave@gmail.com>
2014-04-17 12:37:16 -0400

    cleaned up console functoins

--- src/console.js
+++ src/console.js
@@ -1,31 +1,77 @@
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Custom styling and behavior for browser console statements.
 //------------------------------------------------------------------------------
-d3plus.console = {};
+d3plus.console = function( type , message , style ) {
+  
+  if ( d3plus.ie ) {
 
-d3plus.console.print = function(type,message,style) {
-  if (d3plus.ie) console.log("[d3plus] "+message)
-  else console[type]("%c[d3plus]%c "+message,"font-weight:bold;",style)
+    console.log( "[d3plus] " + message )
+
+  }
+  else {
+
+    var style = style || "font-weight:bold;"
+
+    console[type]( "%c[d3plus]%c " + message
+                 , "font-weight:bold;"
+                 , style )
+
+  }
+
 }
-d3plus.console.log = function(message,style) {
-  if (!style) var style = "font-weight:bold;"
-  d3plus.console.print("log",message,style)
+
+d3plus.console.log = function( message , style ) {
+
+  var style = style || "font-weight:bold;"
+
+  this( "log" , message , style )
+
 }
-d3plus.console.group = function(message,style) {
-  if (!style) var style = "font-weight:bold;"
-  d3plus.console.print("group",message,style)
+
+d3plus.console.group = function( message , style ) {
+
+  var style = style || "font-weight:bold;"
+
+  this( "group" , message , style )
+
 }
-d3plus.console.warning = function(message,style) {
-  if (!style) var style = "font-weight:bold;color:red;"
-  message = "WARNING: "+message
-  d3plus.console.print("log",message,style)
+
+d3plus.console.warning = function( message , style ) {
+
+  var style = style || "font-weight:bold;color:red;"
+
+  message = "WARNING: " + message
+
+  this( "log" , message , style )
+
 }
+
 d3plus.console.groupEnd = function() {
-  if (!d3plus.ie) console.groupEnd()
+
+  if ( !d3plus.ie ) {
+
+    console.groupEnd()
+
+  }
+
 }
-d3plus.console.time = function(message) {
-  if (!d3plus.ie) console.time(message)
+
+d3plus.console.time = function( message ) {
+
+  if ( !d3plus.ie ) {
+
+    console.time( message )
+
+  }
+
 }
-d3plus.console.timeEnd = function(message) {
-  if (!d3plus.ie) console.timeEnd(message)
+
+d3plus.console.timeEnd = function( message ) {
+
+  if ( !d3plus.ie ) {
+
+    console.timeEnd( message )
+
+  }
+
 }
