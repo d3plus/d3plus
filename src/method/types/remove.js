bf5cea9230b043171c08da29057b2ed4c10e6a09
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:43:45 -0400

    created new .remove() method

---
+++ src/method/types/remove.js
@@ -0,0 +1,16 @@
+d3plus.method.remove = {
+  "accepted": undefined,
+  "process": function ( value ) {
+
+    if ( this.initialized ) {
+      var vars = this.getVars()
+      vars.container.value.remove()
+    }
+    else {
+      return value
+    }
+
+
+  },
+  "value": undefined
+}
