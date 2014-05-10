65a55a21451a56dcba27b08f4d3d498c5831715a
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:44:03 -0400

    created new .select() method

---
+++ src/method/types/select.js
@@ -0,0 +1,14 @@
+d3plus.method.select = {
+  "accepted": [ String ],
+  "chainable": false,
+  "process": function ( value ) {
+
+    var vars = this.getVars()
+
+    return vars.container.value && value
+         ? vars.container.value.select(value)
+         : value
+
+  },
+  "value": undefined
+}
