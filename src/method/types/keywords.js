a277d3ac5718f4926b0abfc33fb2e281f64e6d6e
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:43:16 -0400

    created new .keywords() method

---
+++ src/method/types/keywords.js
@@ -0,0 +1,6 @@
+d3plus.method.keywords = {
+  "accepted": [ Array , Boolean , Function , Object , String ],
+  "mute": d3plus.method.filter(true),
+  "solo": d3plus.method.filter(true),
+  "value": "keywords"
+}
