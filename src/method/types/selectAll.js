9543d8337bd00747cb6e70ad843fae7d46ca4fd1
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:44:10 -0400

    created new .selectAll() method

---
+++ src/method/types/selectAll.js
@@ -0,0 +1,14 @@
+d3plus.method.selectAll = {
+  "accepted": [ String ],
+  "chainable": false,
+  "process": function ( value ) {
+
+    var vars = this.getVars()
+
+    return vars.container.value && value
+         ? vars.container.value.selectAll(value)
+         : value
+
+  },
+  "value": undefined
+}
