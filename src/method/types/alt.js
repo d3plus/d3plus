2aadd2c298ad4af432be616270d01e7060ee4548
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:42:58 -0400

    created new .alt() method

---
+++ src/method/types/alt.js
@@ -0,0 +1,6 @@
+d3plus.method.alt = {
+  "accepted": [ Array , Boolean , Function , Object , String ],
+  "mute": d3plus.method.filter(true),
+  "solo": d3plus.method.filter(true),
+  "value": "alt"
+}
