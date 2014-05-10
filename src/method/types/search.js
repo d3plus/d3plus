aa270de16f0b0b7b1e7f80413703ecd846edc452
Dave Landry <landry.dave@gmail.com>
2014-05-10 02:43:54 -0400

    created new .search() method

---
+++ src/method/types/search.js
@@ -0,0 +1,13 @@
+d3plus.method.search = {
+  "accepted": [ "auto" , Boolean ],
+  "process": function(value) {
+
+    if (typeof value == "Boolean") {
+      this.enabled = value
+    }
+
+    return value
+
+  },
+  "value": "auto"
+}
