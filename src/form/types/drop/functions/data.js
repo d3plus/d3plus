328147ea42c00aa157db0fbb58059b6461e2a4a9
Dave Landry <landry.dave@gmail.com>
2014-05-16 14:15:57 -0400

    Fixed #63

--- src/form/types/drop/functions/data.js
+++ src/form/types/drop/functions/data.js
@@ -2,7 +2,7 @@
 // Creates and populates the dropdown list of items.
 //------------------------------------------------------------------------------
 d3plus.input.drop.data = function ( vars ) {
-  
+
   if ( vars.data.url && !vars.data.loaded ) {
     var loadingObject = {}
     loadingObject[vars.text.value] = vars.format.value("Loading...")
@@ -24,31 +24,54 @@ d3plus.input.drop.data = function ( vars ) {
     }
     else {
 
-      vars.data.filtered = vars.data.value.filter(function(d){
+      var exactMatches = []
+        , softMatches  = []
+
+      vars.data.value.forEach(function(d){
 
         var match = false
 
         searchKeys.forEach(function(key){
 
-          if (key in d && d[key]) {
+          if ( !match && key in d && typeof d[key] === "string" ) {
 
-            var text = d3plus.util.strip(d[key].toLowerCase()).split("_")
+            var text = d[key].toLowerCase()
 
-            for (t in text) {
-              for (s in searchWords) {
-                if (text[t].indexOf(searchWords[s]) === 0) {
-                  match = true
+            if ( text.indexOf(searchText) >= 0 ) {
+              exactMatches.push(d)
+              match = true
+            }
+            else {
+
+              var texts = d3plus.util.strip(text).split("_")
+
+              for (t in texts) {
+
+                if ( !match ) {
+
+                  for (s in searchWords) {
+                    if (texts[t].indexOf(searchWords[s]) === 0) {
+                      softMatches.push(d)
+                      match = true
+                      break
+                    }
+                  }
+
+                }
+                else {
                   break
                 }
+
               }
+
             }
           }
 
         })
 
-        return match
-
       })
+
+      vars.data.filtered = exactMatches.concat(softMatches)
 
     }
 
