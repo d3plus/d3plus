19b6b04914443a9d6efd974b7a2dba251ba420a7
Dave Landry <landry.dave@gmail.com>
2014-05-22 10:53:19 -0400

    moved some .coords() params into styles

--- src/method/types/coords.js
+++ src/method/types/coords.js
@@ -1,18 +1,7 @@
 d3plus.method.coords = {
-  "accepted": [ Array , Boolean , Function , Object , String ],
-  "center": [0,0],
-  "fit": {
-    "accepted": ["auto","height","width"],
-    "value": "auto"
-  },
-  "mute": d3plus.method.filter(false),
-  "padding": 20,
-  "process": d3plus.method.processData,
-  "projection": {
-    "accepted": ["mercator","equirectangular"],
-    "value": "mercator"
-  },
-  "solo": d3plus.method.filter(false),
-  "threshold": 0.1,
-  "value": false
+  "accepted" : [ false , Array , Function , Object , String ],
+  "mute"     : d3plus.method.filter(false),
+  "process"  : d3plus.method.processData,
+  "solo"     : d3plus.method.filter(false),
+  "value"    : false
 }
---
+++ src/style/default/params/coords.js
@@ -0,0 +1,13 @@
+d3plus.style.default.coords = {
+  "center"     : [ 0 , 0 ],
+  "fit"        : {
+    "accepted" : [ "auto" , "height" , "width" ],
+    "value"    : "auto"
+  },
+  "padding"    : 20,
+  "projection" : {
+    "accepted" : [ "mercator" , "equirectangular" ],
+    "value"    : "mercator"
+  },
+  "threshold"  : 0.1
+}
