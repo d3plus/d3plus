9aed5f8f1313234a206d9c0b189e6e7491344358
Dave Landry <landry.dave@gmail.com>
2014-06-02 17:38:33 -0400

    fixed problem with d3plus.color.lighter applying a hue to grey

--- src/color/lighter.js
+++ src/color/lighter.js
@@ -1,8 +1,9 @@
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Lightens a color
 //------------------------------------------------------------------------------
-d3plus.color.lighter = function(color,increment) {
-  var c = d3.hsl(color);
+d3plus.color.lighter = function( color , increment ) {
+
+  var c = d3.hsl(color)
 
   if (!increment) {
     var increment = 0.1
@@ -10,12 +11,14 @@ d3plus.color.lighter = function(color,increment) {
 
   c.l += increment
   c.s -= increment/2
-  if (c.l > .95) {
-    c.l = .95
+
+  if (c.l > 1) {
+    c.l = 1
   }
-  if (c.s < .05) {
-    c.s = .05
+  if (c.s < 0) {
+    c.s = 0
   }
 
-  return c.toString();
+  return c.toString()
+
 }
