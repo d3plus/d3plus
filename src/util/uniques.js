e9baf44f4023052d8258322a84ec094c9e2c9ab4
Dave Landry <landry.dave@gmail.com>
2014-06-05 16:52:45 -0400

    catch in d3plus.util.uniques( ) for undefined values

--- src/util/uniques.js
+++ src/util/uniques.js
@@ -1,37 +1,42 @@
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Returns list of unique values
 //------------------------------------------------------------------------------
-d3plus.util.uniques = function(data,value) {
+d3plus.util.uniques = function( data , value ) {
 
-  if (data === undefined) {
+  if ( data === undefined || value === undefined ) {
     return []
   }
 
-  var type = null
+  var type = false
 
-  return d3.nest().key(function(d) {
-      if (typeof value == "string") {
-        if (!type && typeof d[value] !== "undefined") type = typeof d[value]
+  return d3.nest()
+    .key(function(d) {
+
+      if (typeof value === "string") {
+        if ( !type && typeof d[value] !== "undefined" ) type = typeof d[value]
         return d[value]
       }
-      else if (typeof value == "function") {
-        if (!type && typeof value(d) !== "undefined") type = typeof value(d)
+      else if (typeof value === "function") {
+        if ( !type && typeof value(d) !== "undefined" ) type = typeof value(d)
         return value(d)
       }
       else {
         return d
       }
+
     })
     .entries(data)
-    .reduce(function(a,b){
-      if (type) {
-        var val = b.key
-        if (type == "number") val = parseFloat(val)
-        return a.concat(val)
-      }
-      return a
-    },[]).sort(function(a,b){
+    .reduce(function( a , b ){
+
+      return type && b.key !== "undefined"
+           ? a.concat( type === "number" ? parseFloat(b.key) : b.key )
+           : a
+
+    }, [] )
+    .sort(function( a , b ){
+
       return a < b ? -1 : 1
+
     })
 
 }
