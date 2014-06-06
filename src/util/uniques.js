6e10522c1bb658b22f58c93574ba31b81389c236
Dave Landry <landry.dave@gmail.com>
2014-06-06 11:54:23 -0400

    d3plus.util.unitques only sorts if values are numbers

--- src/util/uniques.js
+++ src/util/uniques.js
@@ -8,35 +8,39 @@ d3plus.util.uniques = function( data , value ) {
   }
 
   var type = false
+    , nest = d3.nest()
+        .key(function(d) {
 
-  return d3.nest()
-    .key(function(d) {
+          if (typeof value === "string") {
+            if ( !type && typeof d[value] !== "undefined" ) type = typeof d[value]
+            return d[value]
+          }
+          else if (typeof value === "function") {
+            if ( !type && typeof value(d) !== "undefined" ) type = typeof value(d)
+            return value(d)
+          }
+          else {
+            return d
+          }
 
-      if (typeof value === "string") {
-        if ( !type && typeof d[value] !== "undefined" ) type = typeof d[value]
-        return d[value]
-      }
-      else if (typeof value === "function") {
-        if ( !type && typeof value(d) !== "undefined" ) type = typeof value(d)
-        return value(d)
-      }
-      else {
-        return d
-      }
+        })
+        .entries(data)
+        .reduce(function( a , b ){
 
-    })
-    .entries(data)
-    .reduce(function( a , b ){
+          return type && b.key !== "undefined"
+               ? a.concat( type === "number" ? parseFloat(b.key) : b.key )
+               : a
 
-      return type && b.key !== "undefined"
-           ? a.concat( type === "number" ? parseFloat(b.key) : b.key )
-           : a
+        }, [] )
 
-    }, [] )
-    .sort(function( a , b ){
+  if ( type === "number" ) {
+    nest.sort(function( a , b ){
 
       return a < b ? -1 : 1
 
     })
+  }
+
+  return nest
 
 }
