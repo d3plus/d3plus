e2c587edb56548cedb3b21a09e8609c752979e3d
Dave Landry <landry.dave@gmail.com>
2014-06-06 13:12:04 -0400

    fixed bug detecting text variables at different depths

--- src/variable/text.js
+++ src/variable/text.js
@@ -3,29 +3,31 @@
 //------------------------------------------------------------------------------
 d3plus.variable.text = function(vars,obj,depth) {
 
-  if (typeof depth != "number") var depth = vars.depth.value
+  if ( typeof depth !== "number" ) var depth = vars.depth.value
 
-  if (vars.text.array && typeof vars.text.array == "object") {
-    if (vars.text.array[vars.id.nesting[depth]]) {
-      var text_keys = vars.text.array[vars.id.nesting[depth]]
+  var key = vars.id.nesting[depth]
+
+  if ( vars.text.nesting && d3plus.object.validate(vars.text.nesting) ) {
+    if ( vars.text.nesting[key] ) {
+      var text_keys = vars.text.nesting[key]
     }
     else {
-      var text_keys = vars.text.array[d3.keys(vars.text.array)[0]]
+      var text_keys = vars.text.nesting[d3.keys(vars.text.nesting)[0]]
     }
   }
   else {
     var text_keys = []
     if (vars.text.value) text_keys.push(vars.text.value)
-    text_keys.push(vars.id.nesting[depth])
+    text_keys.push(key)
   }
+
   if (typeof text_keys == "string") {
     text_keys = [text_keys]
   }
 
   var names = []
   text_keys.forEach(function(t){
-    var key =  vars.id.nesting[depth]
-      , name = d3plus.variable.value(vars,obj,t,key)
+    var name = d3plus.variable.value( vars , obj , t , key )
     if (name) names.push(vars.format.value(name.toString()))
   })
 
