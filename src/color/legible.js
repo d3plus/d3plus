316d41b4736a11d60c4d83ff641e60188c6f2b57
Dave Landry <landry.dave@gmail.com>
2014-06-09 16:04:40 -0400

    color legibility tweaks

--- src/color/legible.js
+++ src/color/legible.js
@@ -4,9 +4,9 @@
 d3plus.color.legible = function(color) {
 
   var hsl = d3.hsl(color)
-  if ( hsl.l > .6 ) {
+  if ( hsl.l > .45 ) {
     if ( hsl.s > .8 ) hsl.s = .8
-    hsl.l = 0.6
+    hsl.l = 0.45
   }
   return hsl.toString()
 
