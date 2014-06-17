7cf982914194bc8feadf7c86bf163566c63a28cc
Dave Landry <landry.dave@gmail.com>
2014-06-17 11:40:40 -0400

    setup public d3plus.edges.connections function

---
+++ src/edges/connections.js
@@ -0,0 +1,8 @@
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Returns primary connections for a given node ID
+//------------------------------------------------------------------------------
+d3plus.edges.connections = function( edges , focus , id , source , target , returnNodes ) {
+
+  
+
+}
