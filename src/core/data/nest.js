88d002325551954aee31a7e6be30f164d84dfac8
Dave Landry <landry.dave@gmail.com>
2014-10-31 13:02:57 -0400

    fixes #209

--- src/core/data/nest.js
+++ src/core/data/nest.js
@@ -1,17 +1,18 @@
 var fetchValue = require("../fetch/value.js"),
     validObject  = require("../../object/validate.coffee"),
-    uniqueValues = require("../../util/uniques.coffee")
+    uniqueValues = require("../../util/uniques.coffee");
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Nests and groups the data.
 //------------------------------------------------------------------------------
 var dataNest = function( vars , flatData , nestingLevels , requirements ) {
 
-  var nestedData   = d3.nest()
-    , groupedData  = []
-    , segments     = "temp" in vars ? [ "active" , "temp" , "total" ] : []
-    , requirements = requirements || vars.types[vars.type.value].requirements || []
-    , exceptions   = "time" in vars ? [ vars.time.value , vars.icon.value ] : []
-    , checkAxes    = function() {
+  requirements = requirements || vars.types[vars.type.value].requirements || [];
+
+  var nestedData   = d3.nest(),
+      groupedData  = [],
+      segments     = "temp" in vars ? [ "active" , "temp" , "total" ] : [],
+      exceptions   = "time" in vars ? [ vars.time.value , vars.icon.value ] : [],
+      checkAxes    = function() {
 
       //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       // If the visualization has method requirements, check to see if we need
@@ -21,26 +22,25 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
 
         ["x","y"].forEach(function(axis){
 
-          var axisKey = vars[axis].value
+          var axisKey = vars[axis].value;
 
-          if ( requirements.indexOf(axis) >= 0 && axisKey
-               && vars[axis].scale.value === "discrete") {
+          if (requirements.indexOf(axis) >= 0 && axisKey && vars[axis].scale.value === "discrete") {
 
-            exceptions.push(axisKey)
+            exceptions.push(axisKey);
 
             nestedData.key(function(d){
-              return fetchValue( vars , d , axisKey )
-            })
+              return fetchValue( vars , d , axisKey );
+            });
 
           }
 
-        })
+        });
 
       }
 
-    }
+    };
 
-  if (!(requirements instanceof Array)) requirements = [requirements]
+  if (!(requirements instanceof Array)) requirements = [requirements];
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // Loop through each nesting level.
@@ -54,27 +54,27 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
       .key(function(d){
 
         if ( typeof level === "function" ) {
-          return level(d)
+          return level(d);
         }
 
-        return fetchValue( vars , d , level )
-      })
+        return fetchValue( vars , d , level );
+      });
 
-    checkAxes()
+    checkAxes();
 
-  })
+  });
 
   if ( !nestingLevels.length ) {
 
     nestedData
       .key(function(d){
-        return true
-      })
+        return true;
+      });
 
-    checkAxes()
+    checkAxes();
   }
 
-  var i = nestingLevels.length ? nestingLevels.length - 1 : 0
+  var i = nestingLevels.length ? nestingLevels.length - 1 : 0;
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // If we're at the deepest level, create the rollup function.
@@ -87,8 +87,8 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
     if ( leaves.length === 1 && ("d3plus" in leaves[0]) ) {
       // var returnObj = leaves[0]
       // returnObj.d3plus.depth = i
-      groupedData.push(leaves[0])
-      return leaves[0]
+      groupedData.push(leaves[0]);
+      return leaves[0];
     }
 
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -99,19 +99,19 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
       "d3plus": {
         "depth": i
       }
-    }
+    };
 
     if ("d3plus" in leaves[0]) {
 
       leaves.forEach(function(l){
         if ("d3plus" in l) {
           if (l.d3plus.merged instanceof Array) {
-            if (!returnObj.d3plus.merged) returnObj.d3plus.merged = []
-            returnObj.d3plus.merged = returnObj.d3plus.merged.concat(l.d3plus.merged)
+            if (!returnObj.d3plus.merged) returnObj.d3plus.merged = [];
+            returnObj.d3plus.merged = returnObj.d3plus.merged.concat(l.d3plus.merged);
           }
-          if (l.d3plus.text) returnObj.d3plus.text = l.d3plus.text
+          if (l.d3plus.text) returnObj.d3plus.text = l.d3plus.text;
         }
-      })
+      });
     }
 
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -119,74 +119,73 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
     //--------------------------------------------------------------------------
     segments.forEach(function(c){
 
-      var key = vars[c].value || c
+      var key = vars[c].value || c;
 
       returnObj.d3plus[key] = d3.sum(leaves, function( d ) {
 
+        var a = 0;
+
         if ( vars[c].value ) {
 
-          var a = fetchValue(vars,d,vars[c].value)
+          a = fetchValue(vars,d,vars[c].value);
 
           if ( typeof a !== "number" ) {
-            a = a ? 1 : 0
+            a = a ? 1 : 0;
           }
 
         }
         else if ( c === "total" ) {
-          var a = 1
-        }
-        else {
-          var a = 0
+          a = 1;
         }
 
-        return a
+        return a;
 
-      })
+      });
 
-    })
+    });
 
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     // Aggregate all values detected in the data.
     //--------------------------------------------------------------------------
     for ( var key in vars.data.keys ) {
 
-      var uniques = uniqueValues(leaves,key)
+      var uniques = uniqueValues(leaves,key);
 
       if (uniques.length) {
 
-        var agg     = vars.aggs && vars.aggs.value[key] ? vars.aggs.value[key] : "sum"
-          , aggType = typeof agg
-          , keyType = vars.data.keys[key]
-          , idKey   = vars.id.nesting.indexOf(key) >= 0
-          , timeKey = "time" in vars && key === vars.time.value
+        var agg     = vars.aggs && vars.aggs.value[key] ? vars.aggs.value[key] : "sum",
+            aggType = typeof agg,
+            keyType = vars.data.keys[key],
+            idKey   = vars.id.nesting.indexOf(key) >= 0,
+            timeKey = "time" in vars && key === vars.time.value;
 
         if ( key in returnObj.d3plus ) {
 
-          returnObj[key] = returnObj.d3plus[key]
+          returnObj[key] = returnObj.d3plus[key];
 
         }
         else if ( aggType === "function" ) {
 
-          returnObj[key] = vars.aggs.value[key](leaves)
+          returnObj[key] = vars.aggs.value[key](leaves);
 
         }
         else if ( timeKey ) {
-          var dates = []
+          var dates = [];
 
           function parseDates(arr) {
 
             for ( var i = 0; i < arr.length ; i++ ) {
-              var d = arr[i]
+              var d = arr[i];
               if (d !== undefined) {
-                if (d.constructor === Date) dates.push(d)
+                if (d.constructor === Date) dates.push(d);
                 else if (d.constructor === Array) {
-                  parseDates(d)
+                  parseDates(d);
                 }
                 else {
-                  d = new Date(d.toString())
+                  d = new Date(d.toString());
                   if (d !== "Invalid Date") {
-                    d.setTime( d.getTime() + d.getTimezoneOffset() * 60 * 1000 )
-                    dates.push(d)
+                    d.setTime( d.getTime() + d.getTimezoneOffset() * 60 * 1000 );
+                    dates.push(d);
                   }
                 }
               }
@@ -194,55 +193,55 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
 
           }
 
-          parseDates(uniques)
+          parseDates(uniques);
 
-          if (dates.length === 1) returnObj[key] = dates[0]
-          else returnObj[key] = dates
+          if (dates.length === 1) returnObj[key] = dates[0];
+          else returnObj[key] = dates;
 
         }
         else if ( keyType === "number" && aggType === "string" && !idKey ) {
-          returnObj[key] = d3[agg](uniques)
+          returnObj[key] = d3[agg](leaves.map(function(d){return d[key];}));
         }
         else {
 
-          var testVals = []
+          var testVals = [];
           function checkVal(obj) {
             if (obj instanceof Array) {
-              obj.forEach(checkVal)
+              obj.forEach(checkVal);
             }
             else if (validObject(obj) && key in obj) {
               if (obj[key] instanceof Array) {
-                obj[key].forEach(checkVal)
+                obj[key].forEach(checkVal);
               }
               else {
-                testVals.push(obj)
+                testVals.push(obj);
               }
             }
           }
-          checkVal(leaves)
+          checkVal(leaves);
 
           var keyValues = testVals.length === 1 ? testVals[0][key]
-                        : uniqueValues( testVals , key )
+                        : uniqueValues( testVals , key );
 
           if ( keyValues !== undefined && keyValues !== null ) {
 
             if ( !(keyValues instanceof Array) ) {
-              keyValues = [ keyValues ]
+              keyValues = [ keyValues ];
             }
 
             if ( keyValues.length ) {
 
               if ( idKey && vars.id.nesting.indexOf(key) > i && testVals.length > 1 ) {
                 if (key == "id" && nestingLevels.length == 1 && testVals.length > leaves.length) {
-                  var newNesting = nestingLevels.concat(key)
-                  testVals = dataNest(vars,testVals,newNesting)
+                  var newNesting = nestingLevels.concat(key);
+                  testVals = dataNest(vars,testVals,newNesting);
                 }
-                returnObj[key] = testVals.length === 1 ? testVals[0] : testVals
+                returnObj[key] = testVals.length === 1 ? testVals[0] : testVals;
 
               }
               else {
 
-                returnObj[key] = keyValues.length === 1 ? keyValues[0] : keyValues
+                returnObj[key] = keyValues.length === 1 ? keyValues[0] : keyValues;
 
               }
 
@@ -250,9 +249,9 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
 
           }
           else if (idKey) {
-            var endPoint = vars.id.nesting.indexOf(key) - 1
+            var endPoint = vars.id.nesting.indexOf(key) - 1;
             if (endPoint >= i && (!("endPoint" in returnObj.d3plus) || returnObj.d3plus.endPoint > i)) {
-              returnObj.d3plus.endPoint = i
+              returnObj.d3plus.endPoint = i;
             }
           }
 
@@ -262,55 +261,55 @@ var dataNest = function( vars , flatData , nestingLevels , requirements ) {
 
     }
 
-    groupedData.push(returnObj)
+    groupedData.push(returnObj);
 
-    return returnObj
+    return returnObj;
 
-  })
+  });
 
   var rename_key_value = function(obj) {
     if (obj.values && obj.values.length) {
       obj.children = obj.values.map(function(obj) {
         return rename_key_value(obj);
-      })
-      delete obj.values
-      return obj
+      });
+      delete obj.values;
+      return obj;
     }
     else if(obj.values) {
-      return obj.values
+      return obj.values;
     }
     else {
       return obj;
     }
-  }
+  };
 
   var find_keys = function(obj,depth,keys) {
     if (obj.children) {
       if (vars.data.keys[nestingLevels[depth]] == "number") {
-        obj.key = parseFloat(obj.key)
+        obj.key = parseFloat(obj.key);
       }
-      keys[nestingLevels[depth]] = obj.key
-      delete obj.key
+      keys[nestingLevels[depth]] = obj.key;
+      delete obj.key;
       for ( var k in keys ) {
-        obj[k] = keys[k]
+        obj[k] = keys[k];
       }
-      depth++
+      depth++;
       obj.children.forEach(function(c){
-        find_keys(c,depth,keys)
-      })
+        find_keys(c,depth,keys);
+      });
     }
-  }
+  };
 
   nestedData = nestedData
     .entries(flatData)
     .map(rename_key_value)
     .map(function(obj){
-      find_keys(obj,0,{})
-      return obj
-    })
+      find_keys(obj,0,{});
+      return obj;
+    });
 
-  return groupedData
+  return groupedData;
 
-}
+};
 
-module.exports = dataNest
+module.exports = dataNest;
