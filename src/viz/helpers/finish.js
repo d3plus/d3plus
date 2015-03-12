d643fbcc939fae2414c495eb88cbde296e6593b4
Dave Landry <landry.dave@gmail.com>
2015-03-12 14:11:49 -0400

    zoom reset no longer transitions

--- src/viz/helpers/finish.js
+++ src/viz/helpers/finish.js
@@ -24,11 +24,11 @@ module.exports = function(vars) {
 
       if ( vars.dev.value ) print.time("calculating zoom")
 
-      if (vars.draw.first) {
-        bounds(vars,zoom,0);
+      if (vars.draw.first || vars.zoom.reset) {
+        bounds(vars, zoom, 0);
       }
-      else if (vars.zoom.reset || vars.type.changed || vars.focus.changed || vars.height.changed || vars.width.changed || vars.nodes.changed || vars.legend.changed || vars.timeline.changed || vars.ui.changed) {
-        bounds(vars,zoom);
+      else if (vars.type.changed || vars.focus.changed || vars.height.changed || vars.width.changed || vars.nodes.changed || vars.legend.changed || vars.timeline.changed || vars.ui.changed) {
+        bounds(vars, zoom);
       }
 
       if ( vars.dev.value ) print.timeEnd("calculating zoom")
