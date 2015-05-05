b3885b7fe4fb2c72c3c467b51e68405afff7ca51
Dave Landry <landry.dave@gmail.com>
2015-05-05 17:05:54 -0400

    removed extra % in total sub-title

--- src/viz/helpers/ui/titles.js
+++ src/viz/helpers/ui/titles.js
@@ -71,7 +71,7 @@ module.exports = function(vars) {
           var pct = (total/overall_total)*100,
               ot = vars.format.value(overall_total, {"key": vars.size.value, "vars": vars});
 
-          pct = " ("+vars.format.value(pct,{"key": "share", "vars": vars})+"% of "+ot+")";
+          pct = " ("+vars.format.value(pct,{"key": "share", "vars": vars})+" of "+ot+")";
 
         }
       }
