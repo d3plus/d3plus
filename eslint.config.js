1d59ed1d16ad4548a3aff7d843007d9eb245b67e
Dave Landry <landry.dave@gmail.com>
2025-03-07 12:28:07 -0500

    fixes global eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,13 @@
 import js from "@eslint/js";
-export default [js.configs.recommended];
+// export default [js.configs.recommended];
+export default {
+  plugins: {
+    js
+  },
+  languageOptions: {
+    globals: {
+      browser: true,
+      node: true
+    }
+  }
+};
