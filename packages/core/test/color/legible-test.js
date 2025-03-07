b66dab7f27e14c1abdc27533b8166717bfd1334d
Dave Landry <landry.dave@gmail.com>
2025-03-07 09:44:36 -0500

    cleans up unused files

--- packages/core/test/color/legible-test.js
+++
@@ -1,9 +0,0 @@
-import assert from "assert";
-import {default as legible} from "../../src/color/legible.js";
-
-it("legible", () => {
-  assert.strictEqual("rgb(207, 23, 23)", legible("#ffaaaa"));
-  assert.strictEqual("rgb(23, 207, 23)", legible("#ccffcc"));
-  assert.strictEqual("rgb(23, 23, 207)", legible("#ccccff"));
-});
-
--- packages/core/test/color/lighter-test.js
+++
@@ -1,9 +0,0 @@
-import assert from "assert";
-import {default as lighter} from "../../src/color/lighter.js";
-
-it("lighter", () => {
-  assert.strictEqual("rgb(207, 82, 82)", lighter("#440000"));
-  assert.strictEqual("rgb(82, 207, 82)", lighter("#004400"));
-  assert.strictEqual("rgb(82, 82, 207)", lighter("#000044"));
-});
-
