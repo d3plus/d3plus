ffabc3bcb3a296bc614e82aec8ee34822d3fff89
Dave Landry <landry.dave@gmail.com>
2025-06-25 16:26:19 -0400

    fixes Radar example

--- packages/docs/packages/core/charts/Radar.stories.jsx
+++ packages/docs/packages/core/charts/Radar.stories.jsx
@@ -28,33 +28,21 @@ const Template = (args) => <Radar config={configify(args, argTypes)} />;
 export const BasicExample = Template.bind({});
 BasicExample.args = {
   data: [
-    {element: "alpha",   birth: 2004, death: 2007},
-    {element: "epsilon", birth: 2007, death: 2012},
-    {element: "beta",    birth: 2005, death: 2010}
+    {id: "alpha", axis: "Central",    number: 170.992},
+    {id: "alpha", axis: "Kirkdale",   number: 40},
+    {id: "alpha", axis: "Kensington", number: 240},
+    {id: "alpha", axis: "Everton",    number: 90},
+    {id: "alpha", axis: "Picton",     number: 160},
+    {id: "alpha", axis: "Riverside",  number: 30},
+    {id: "beta",  axis: "Central",    number: 320},
+    {id: "beta",  axis: "Kirkdale",   number: 97.5},
+    {id: "beta",  axis: "Kensington", number: 40},
+    {id: "beta",  axis: "Everton",    number: 110},
+    {id: "beta",  axis: "Picton",     number: 40},
+    {id: "beta",  axis: "Riverside",  number: 110}
   ],
-  end: "death",
-  groupBy: "element",
-  start: "birth"
+  groupBy: "id",
+  metric: "axis",
+  value: "number"
 };
-BasicExample.parameters = {controls: {include: ["end", "start"]}};
-
-export const GroupingBarsIntoLanes = Template.bind({});
-GroupingBarsIntoLanes.args = {
-  data: [
-    {parent: "Group 1", id: "alpha",   start: 2004, end: 2007},
-    {parent: "Group 2", id: "epsilon", start: 2007, end: 2012},
-    {parent: "Group 1", id: "beta",    start: 2005, end: 2010},
-    {parent: "Group 1", id: "gamma",   start: 2008, end: 2013},
-    {parent: "Group 2", id: "delta",   start: 2004, end: 2007}
-  ],
-  end: "death",
-  groupBy: ["parent", "id"],
-  shapeConfig: {
-    fill: funcify(
-      d => d.parent === "Group 1" ? "firebrick" : "cornflowerblue",
-      `d => d.parent === "Group 1" ? "firebrick" : "cornflowerblue"`
-    )
-  },
-  start: "birth"
-};
-GroupingBarsIntoLanes.parameters = {controls: {include: ["groupBy", "shapeConfig"]}};
+BasicExample.parameters = {controls: {include: ["metric", "value"]}};
