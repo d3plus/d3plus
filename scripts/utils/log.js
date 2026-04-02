0a09e0d36e71d7e958894d9c179b2ef817280d7c
Dave Landry <landry.dave@gmail.com>
2026-04-02 16:39:16 -0400

    rewrites logger to use yocto-spinner and removes chalk dep

--- scripts/utils/log.js
+++ scripts/utils/log.js
@@ -1,83 +1,62 @@
-import chalk from "chalk";
-import shell from "shelljs";
+import fs from "node:fs";
+import yoctoSpinner from "yocto-spinner";
 
-const {name, version} = JSON.parse(shell.cat("package.json"));
+const {name, version} = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
-const frames = ["▁", "▄", "▆", "█", "█", "▆", "▅", "▁"].map(d => chalk.dim(d));
-const color = [36, 114, 200];
+const rgb = (r, g, b, text) => `\x1b[38;2;${r};${g};${b}m${text}\x1b[0m`;
+const bold = text => `\x1b[1m${text}\x1b[22m`;
 
 export default function (script) {
+  const header = process.env.SUBPROCESS
+    ? `${bold(rgb(36, 114, 200, name))} - ${rgb(36, 114, 200, script)}`
+    : `${bold(rgb(36, 114, 200, `${name} v${version}`))}\n      ${rgb(36, 114, 200, script)}`;
+
+  yoctoSpinner().info(header);
+
+  let spinner = null;
+
   const Logger = {};
 
-  if (process.env.SUBPROCESS) {
-    shell.echo(`
-${chalk.bold.rgb(...color)(name)} - ${chalk.rgb(...color)(script)}`);
-  } else {
-    shell.echo(`
-  
-      ${chalk.bold.rgb(...color)(`${name} v${version}`)}
-      ${chalk.rgb(...color)(script)}
-  
-    `);
-  }
-
-  let interval,
-    message = "";
-
   Logger.done = msg => {
-    if (msg) message = msg;
-    interval = clearInterval(interval);
-    shell.echo(`\r[ ${chalk.rgb(...color)("done")} ] ${message}`);
-  };
-
-  Logger.fail = msg => {
-    if (msg) message = msg;
-    interval = clearInterval(interval);
-    shell.echo(`\r[ ${chalk.red("fail")} ] ${message}`);
-  };
-
-  Logger.exit = msg => {
-    if (interval) Logger.done(msg);
-    shell.echo("\n");
-  };
-
-  Logger.timer = (
-    msg = chalk.gray("please pass a process name to .start()"),
-  ) => {
-    if (interval) Logger.done();
-
-    message = msg;
-
-    process.stdout.write(`\r[ ${chalk.dim("····")} ] ${message}`);
-
-    if (process.env.GITHUB_ACTIONS !== "true") {
-      let tick = 0;
-
-      interval = setInterval(() => {
-        const arr = Array(4);
-        frames.forEach((f, i) => {
-          const index = (i + (tick % frames.length)) % frames.length;
-          if (index < arr.length) arr[index] = f;
-        });
-        process.stdout.write(
-          `\r[ ${chalk.rgb(...color)(arr.join(""))} ] ${message}`,
-        );
-        tick++;
-      }, 50);
-    } else {
-      interval = true;
+    if (spinner) {
+      spinner.success(msg || spinner.text);
+      spinner = null;
     }
   };
 
+  Logger.fail = msg => {
+    if (spinner) {
+      spinner.error(msg || spinner.text);
+      spinner = null;
+    }
+  };
+
+  Logger.exit = msg => {
+    if (spinner) Logger.done(msg);
+    console.log();
+  };
+
+  Logger.timer = msg => {
+    if (spinner) Logger.done();
+    spinner = yoctoSpinner({
+      text: msg || "please pass a process name to .start()",
+      color: "cyan",
+    }).start();
+  };
+
   Logger.update = msg => {
-    if (msg) message = msg;
-    process.stdout.write(`\r[ ${chalk.gray("····")} ] ${message}`);
+    if (spinner) {
+      spinner.text = msg;
+    } else {
+      spinner = yoctoSpinner({text: msg, color: "cyan"}).start();
+    }
   };
 
   Logger.warn = msg => {
-    if (msg) message = msg;
-    interval = clearInterval(interval);
-    shell.echo(`\r[ ${chalk.yellow("warn")} ] ${message}`);
+    if (spinner) {
+      spinner.warning(msg || spinner.text);
+      spinner = null;
+    }
   };
 
   return Logger;
