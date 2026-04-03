562005f41b7a77f9563fb91745ec8d81cdab97ad
Dave Landry <landry.dave@gmail.com>
2026-04-03 13:20:21 -0400

    removes d3-selection as dependency of rtl

--- packages/dom/src/rtl.ts
+++ packages/dom/src/rtl.ts
@@ -1,11 +1,11 @@
-import {select} from "d3-selection";
-
 /**
     Returns `true` if the HTML or body element has either the "dir" HTML attribute or the "direction" CSS property set to "rtl".
 */
 export default function (): boolean {
-  return select("html").attr("dir") === "rtl" ||
-    select("body").attr("dir") === "rtl" ||
-    select("html").style("direction") === "rtl" ||
-    select("body").style("direction") === "rtl";
+  return (
+    document.documentElement.dir === "rtl" ||
+    document.body.dir === "rtl" ||
+    getComputedStyle(document.documentElement).direction === "rtl" ||
+    getComputedStyle(document.body).direction === "rtl"
+  );
 }
